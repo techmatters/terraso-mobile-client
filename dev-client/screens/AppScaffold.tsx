4ed3a704efeecc7f0d79aa8997bd124847947562
David Code Howard <david@techmatters.org>
2023-06-09 11:52:20 -0400

    feat: Re-display react-navigation header
    Originally I disabled the react-navigation because I did not know how
    to style it properly. Now that the screens are using some more
    advanced features, rather than just having the menu bar and the info
    icon, it will be much easier to use the built in header. This commit
    takes care of the background room to do this, mainly massaging a bit
    the typing of different screen constructs, setting up the screen
    config to display different options on different screens, and
    correctly styling the different screen titles. Will still need to
    remove the default AppBar component and style the header properly.

--- dev-client/screens/AppScaffold.tsx
+++ dev-client/screens/AppScaffold.tsx
@@ -1,55 +1,79 @@
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {
+  NativeStackNavigationOptions,
+  createNativeStackNavigator,
+} from '@react-navigation/native-stack';
 import {RootStackParamList, ScreenRoutes} from './constants';
 import {useLogin} from '../context/LoginContext';
-import SCREENS, {LoggedOut} from '.';
+import SCREENS, {ScreenConfig} from '.';
 import {fetchProjects, SITE_DISPLAYS} from '../dataflow';
+import {useTranslation} from 'react-i18next';
+import {TFunction} from 'i18next';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-type ScreenMapArgs = [ScreenRoutes, any];
+type ScreenMapArgs = [ScreenRoutes, ScreenConfig<keyof RootStackParamList>];
 
 const previews = fetchProjects();
 const sites = SITE_DISPLAYS;
 
-function mapScreens([name, component]: ScreenMapArgs) {
-  // TODO: This is a stub that will be removed in connection with backend
-  let initialParams;
-  switch (name) {
-    case 'PROJECT_LIST':
-      initialParams = {projects: previews};
-      break;
-    case 'SITES_MAP':
-      initialParams = {sites: sites};
-  }
-  return (
-    <Stack.Screen
-      name={name}
-      component={component}
-      initialParams={initialParams}
-      key={name}
-    />
-  );
+function mapScreens(t: TFunction<'translation', undefined, 'translation'>) {
+  return ([name, config]: ScreenMapArgs) => {
+    // TODO: initialParams are stubs, a stopgap for while we are not connected to the backend
+    // This setup should be changed when we get to connecting the backend
+    let initialParams;
+    switch (name) {
+      case 'PROJECT_LIST':
+        initialParams = {projects: previews};
+        break;
+      case 'SITES_MAP':
+        initialParams = {sites: sites};
+    }
+
+    let options: NativeStackNavigationOptions = {};
+
+    if (config.hideBack) {
+      options.headerBackVisible = false;
+    }
+
+    options.headerLeft = config.leftIcon;
+    options.headerRight = config.rightIcon;
+
+    return (
+      <Stack.Screen
+        name={name}
+        component={config.screen}
+        initialParams={initialParams}
+        key={name}
+        options={({route}) => {
+          if (config.paramTitle !== undefined) {
+            options.headerTitle = config.paramTitle({t, route});
+          } else {
+            options.headerTitle = t('screens.' + name) ?? undefined;
+          }
+          return options;
+        }}
+      />
+    );
+  };
 }
 
 export default function AppScaffold() {
   const {user} = useLogin();
+  const {t} = useTranslation();
 
-  function filterLogin([name, _]: ScreenMapArgs) {
+  function filterLogin([_name, config]: ScreenMapArgs) {
     if (user === null) {
-      return LoggedOut.has(name);
+      return config.loggedOut === true;
     } else {
-      return !LoggedOut.has(name);
+      return config.loggedOut !== true;
     }
   }
+
   return (
-    <Stack.Navigator
-      initialRouteName={ScreenRoutes.SITES_MAP}
-      screenOptions={{
-        headerShown: false,
-      }}>
-      {(Object.entries(SCREENS) as [ScreenRoutes, any])
+    <Stack.Navigator initialRouteName={ScreenRoutes.SITES_MAP}>
+      {(Object.entries(SCREENS) as ScreenMapArgs[])
         .filter(filterLogin)
-        .map(mapScreens)}
+        .map(mapScreens(t))}
     </Stack.Navigator>
   );
 }
