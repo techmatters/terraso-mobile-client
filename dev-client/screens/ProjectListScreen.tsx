99a99be9d77c8aa541d9c505870df89d9ab6edbc
David Code Howard <david@techmatters.org>
2023-06-19 16:43:42 -0400

    feature: Change app bar (#81)
    * feat: Re-display react-navigation header
    
    Originally I disabled the react-navigation because I did not know how
    to style it properly. Now that the screens are using some more
    advanced features, rather than just having the menu bar and the info
    icon, it will be much easier to use the built in header. This commit
    takes care of the background room to do this, mainly massaging a bit
    the typing of different screen constructs, setting up the screen
    config to display different options on different screens, and
    correctly styling the different screen titles. Will still need to
    remove the default AppBar component and style the header properly.
    
    * feat: Remove AppBar, style header

--- dev-client/screens/ProjectListScreen.tsx
+++ dev-client/screens/ProjectListScreen.tsx
@@ -2,7 +2,6 @@ import {RootStackParamList, ScreenRoutes} from './constants';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import ProjectListView from '../components/projects/ProjectListView';
 import {VStack} from 'native-base';
-import AppBar from '../components/AppBar';
 import BottomNavigation from '../components/common/BottomNavigation';
 import React from 'react';
 import {useTranslation} from 'react-i18next';
@@ -17,7 +16,6 @@ export default function ProjectListScreen({route, navigation}: Props) {
 
   return (
     <VStack height="100%">
-      <AppBar title={t('projects.title')} />
       <ProjectListView projects={route.params.projects} />
       <BottomNavigation />
     </VStack>
