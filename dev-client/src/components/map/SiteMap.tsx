2226888e0bd02ec45ed82837965679a7f5fbaa1e
garo (they/them) <3411715+shrouxm@users.noreply.github.com>
2023-07-07 07:53:58 -0700

    feat: show home screen site callouts, use redux store (#154)
    * feat: show home screen site callouts, use redux store
    
    * feat: update client shared version

--- dev-client/src/components/map/SiteMap.tsx
+++ dev-client/src/components/map/SiteMap.tsx
@@ -1,23 +1,76 @@
-import Mapbox, {Camera, MarkerView, UserLocation} from '@rnmapbox/maps';
-import React, {memo, useEffect, useRef, useState} from 'react';
+import Mapbox, {Camera, UserLocation} from '@rnmapbox/maps';
+import {OnPressEvent} from '@rnmapbox/maps/src/types/OnPressEvent';
+import {memo, useEffect, useMemo, useRef, useState} from 'react';
 // TODO: Is it better to import type?
 import {type Position} from '@rnmapbox/maps/lib/typescript/types/Position';
 import MaterialIconButton from '../common/MaterialIconButton';
 import {v4 as uuidv4} from 'uuid';
-
-export type SiteMarker = {
-  id: string;
-  coordinates: GeoJSON.Position;
-  popup?: JSX.Element;
-};
+import {useSelector} from '../../model/store';
+import {Site} from 'terraso-client-shared/site/siteSlice';
+import {Box, Heading, Text} from 'native-base';
 
 type SiteMapProps = {
-  sites: SiteMarker[];
   center?: Position;
 };
 
-const SiteMap = memo(({sites, center}: SiteMapProps): JSX.Element => {
-  const [temporarySites, setTemporarySites] = useState<SiteMarker[]>([]);
+const siteFeatureCollection = (
+  sites: Site[],
+): GeoJSON.FeatureCollection<GeoJSON.Geometry> => ({
+  type: 'FeatureCollection',
+  features: sites.map(site => ({
+    type: 'Feature',
+    id: site.id,
+    properties: {},
+    geometry: {
+      type: 'Point',
+      coordinates: [site.longitude, site.latitude],
+    },
+  })),
+});
+
+const SiteCallout = ({site}: {site: Site}): JSX.Element => {
+  return (
+    <Mapbox.MarkerView
+      coordinate={[site.longitude, site.latitude]}
+      anchor={{x: 0.5, y: 0}}
+      allowOverlap={true}>
+      <Box backgroundColor="background.default">
+        <Heading size="md">{site.name}</Heading>
+        <Text>latitude: {site.latitude}</Text>
+        <Text>longitude: {site.longitude}</Text>
+      </Box>
+    </Mapbox.MarkerView>
+  );
+};
+
+const TemporarySiteCallout = ({site}: {site: Site}): JSX.Element => {
+  return (
+    <Mapbox.MarkerView
+      coordinate={[site.longitude, site.latitude]}
+      anchor={{x: 0.5, y: 0}}
+      allowOverlap={true}>
+      <Box backgroundColor="background.default">
+        <Text>latitude: {site.latitude} </Text>
+        <Text>longitude: {site.longitude}</Text>
+      </Box>
+    </Mapbox.MarkerView>
+  );
+};
+
+const SiteMap = memo(({center}: SiteMapProps): JSX.Element => {
+  const sites = useSelector(state => state.site.sites);
+  const [temporarySites, setTemporarySites] = useState<Record<string, Site>>(
+    {},
+  );
+  const [selectedSiteID, setSelectedSiteID] = useState<string | null>(null);
+  const [selectedTemporarySiteID, setSelectedTemporarySiteID] = useState<
+    string | null
+  >(null);
+  const selectedSite = selectedSiteID === null ? null : sites[selectedSiteID];
+  const selectedTemporarySite =
+    selectedTemporarySiteID === null
+      ? null
+      : temporarySites[selectedTemporarySiteID];
   const camera = useRef<Camera>(null);
 
   useEffect(() => {
@@ -26,20 +79,29 @@ const SiteMap = memo(({sites, center}: SiteMapProps): JSX.Element => {
     });
   }, [center]);
 
-  let markers = sites.concat(temporarySites).map(site => {
-    return (
-      <MarkerView
-        coordinate={site.coordinates}
-        key={site.id}
-        allowOverlap={true}>
-        <MaterialIconButton
-          name="location-on"
-          iconButtonProps={{size: 'sm'}}
-          iconProps={{color: 'secondary.main'}}
-        />
-      </MarkerView>
-    );
-  });
+  const sitesFeature = useMemo(
+    () =>
+      siteFeatureCollection(
+        Object.values(sites).filter(site => !site.archived),
+      ),
+    [sites],
+  );
+  console.log(sites, sitesFeature);
+
+  const temporarySitesFeature = useMemo(
+    () => siteFeatureCollection(Object.values(temporarySites)),
+    [temporarySites],
+  );
+
+  const onSitePress = (event: OnPressEvent) => {
+    setSelectedSiteID(event.features[0].id as string);
+    setSelectedTemporarySiteID(null);
+  };
+
+  const onTemporarySitePress = (event: OnPressEvent) => {
+    setSelectedTemporarySiteID(event.features[0].id as string);
+    setSelectedSiteID(null);
+  };
 
   const onLongPress = (feature: GeoJSON.Feature) => {
     if (feature.geometry === null || feature.geometry.type !== 'Point') {
@@ -49,11 +111,15 @@ const SiteMap = memo(({sites, center}: SiteMapProps): JSX.Element => {
       );
       return;
     }
-    const siteMarker: SiteMarker = {
+    const [lon, lat] = feature.geometry.coordinates;
+    const site: Site = {
       id: uuidv4(),
-      coordinates: feature.geometry.coordinates,
+      name: 'temporary site',
+      latitude: lat,
+      longitude: lon,
+      archived: false,
     };
-    setTemporarySites([...temporarySites, siteMarker]);
+    setTemporarySites({...temporarySites, [site.id]: site});
   };
 
   return (
@@ -64,10 +130,39 @@ const SiteMap = memo(({sites, center}: SiteMapProps): JSX.Element => {
       }}
       onLongPress={onLongPress}>
       <Camera ref={camera} centerCoordinate={[0, 0]} />
+      <Mapbox.Images>
+        <Mapbox.Image name="sitePin">
+          <MaterialIconButton name="location-on" />
+        </Mapbox.Image>
+      </Mapbox.Images>
+      <Mapbox.ShapeSource
+        id="sitesSource"
+        shape={sitesFeature}
+        onPress={onSitePress}>
+        <Mapbox.SymbolLayer id="sitesLayer" style={styles.siteLayer} />
+      </Mapbox.ShapeSource>
+      <Mapbox.ShapeSource
+        id="temporarySitesSource"
+        shape={temporarySitesFeature}
+        onPress={onTemporarySitePress}>
+        <Mapbox.SymbolLayer id="temporarySitesLayer" style={styles.siteLayer} />
+      </Mapbox.ShapeSource>
       <UserLocation />
-      {markers}
+      {selectedSite && <SiteCallout site={selectedSite} />}
+      {selectedTemporarySite && (
+        <TemporarySiteCallout site={selectedTemporarySite} />
+      )}
     </Mapbox.MapView>
   );
 });
+
+const styles = {
+  siteLayer: {
+    iconAllowOverlap: true,
+    iconAnchor: 'bottom',
+    iconSize: 1.0,
+    iconImage: 'sitePin',
+  } satisfies Mapbox.SymbolLayerStyle,
+};
 
 export default SiteMap;
--- dev-client/src/screens/SiteMapScreen.tsx
+++ dev-client/src/screens/SiteMapScreen.tsx
@@ -1,21 +1,21 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '.';
 import {ScreenRoutes} from './constants';
-import SiteMap, {SiteMarker} from '../components/map/SiteMap';
+import SiteMap from '../components/map/SiteMap';
 import BottomNavigation from '../components/common/BottomNavigation';
 import {Box, VStack} from 'native-base';
+// import {useFetchData} from 'terraso-client-shared/store/utils';
+// import {fetchSitesForUser} from 'terraso-client-shared/site/siteSlice';
 
 type Props = NativeStackScreenProps<RootStackParamList, ScreenRoutes.SITES_MAP>;
 
-export default function SiteMapScreen({route}: Props) {
-  const siteMarkers = route.params.sites.map<SiteMarker>(site => ({
-    id: site.id.toString(),
-    coordinates: [site.lon, site.lat],
-  }));
+export default function SiteMapScreen(_: Props) {
+  // useFetchData(fetchSitesForUser);
+
   return (
     <VStack>
       <Box flexBasis="90%">
-        <SiteMap sites={siteMarkers} />
+        <SiteMap />
       </Box>
       <Box flexBasis="10%">
         <BottomNavigation />
