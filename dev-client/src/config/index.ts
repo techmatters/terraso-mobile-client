a062b5c64394e56c193693c699f5b0f45910f43e
David Code Howard <david@techmatters.org>
2023-07-07 16:04:57 -0400

    feature: Signin (#126)
    * feat: Get log in token through Google
    
    * feat: "sign" user in
    
    This PR uses the cribbed context I originally created to query the
    backend, grab tokens, and store the user name. It is not complete
    because it doesn't integrate with the GraphQL API stuff yet. I think
    this stuff is best tackled in a separate PR.
    
    * feat: Initialize API config
    
    * chore: save
    
    * feat: Change login display to use redux store
    
    * feat: Properly handle redux + token storage
    
    * fix: Address merge comments
    
    * chore: Fix lint errors
    
    * chore: Fix import errors
    
    * chore: Update package-lock.json
    
    * chore: Fix merge conflict messup
    
    * chore: Change encrypted storage module
    
    * fix: Temp fix to stop map from crashing

---
+++ dev-client/src/config/index.ts
@@ -0,0 +1,41 @@
+import Config from 'react-native-config';
+import {MMKVLoader} from 'react-native-mmkv-storage';
+import {setAPIConfig, TerrasoAPIConfig} from 'terraso-client-shared/config';
+
+const terrasoAPIURL =
+  Config.TERRASO_BACKEND ?? 'https://api.staging.terraso.net';
+
+const MMKV = new MMKVLoader().withEncryption().initialize();
+
+const apiConfig = setAPIConfig({
+  terrasoAPIURL: terrasoAPIURL,
+  graphQLEndpoint: terrasoAPIURL + '/graphql',
+  tokenStorage: {
+    getToken: async name => {
+      const value = await MMKV.getStringAsync(name);
+      return value === null ? undefined : value;
+    },
+    setToken: (name, value) => MMKV.setStringAsync(name, value),
+    removeToken: name => {
+      MMKV.removeItem(name);
+    },
+    initialToken: null,
+  },
+  // TODO: pick out logger
+  logger: (_severity, args) => console.log(args),
+} as TerrasoAPIConfig);
+
+export const getConfig = apiConfig;
+
+// NOTE: This will be changed to be more general at some point, for now it's just grabbing
+// values from the config setup
+type AppConfig = {
+  packageName: string;
+  googleClientId: string;
+};
+
+export const APP_CONFIG: AppConfig = {
+  packageName: 'org.terraso.landpks',
+  googleClientId:
+    (Config.GOOGLE_OAUTH_APP_GUID ?? '') + '.apps.googleusercontent.com',
+};
