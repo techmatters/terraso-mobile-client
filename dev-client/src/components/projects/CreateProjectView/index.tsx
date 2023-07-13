0447b6e8c295e0aab014a8a6f6554715fab75b1c
David Code Howard <david@techmatters.org>
2023-07-13 12:52:11 -0400

    feat: Split CreateProjectScreen into screen and view component
    I think this pattern is best to keep the screen managing global redux
    state. That way the "View" component doesn't need to worry about
    fetching data and updating data, and instead can take data from props.

---
+++ dev-client/src/components/projects/CreateProjectView/index.tsx
@@ -0,0 +1,34 @@
+import {Box, HStack, Input, Text, VStack} from 'native-base';
+import RadioBlock from '../../common/RadioBlock';
+import MaterialIconButton from '../../common/MaterialIconButton';
+import SaveFAB from '../../common/SaveFAB';
+import {useTranslation} from 'react-i18next';
+
+export default function CreateProjectView() {
+  const {t} = useTranslation();
+  return (
+    <Box height="100%" pt="20%" bgColor="background.default">
+      <VStack space={6} mx={5}>
+        <Input placeholder={t('projects.add.name')} />
+        <Input placeholder={t('projects.add.description')} />
+        <RadioBlock<'public' | 'private'>
+          label={
+            <HStack alignItems="center">
+              <Text>Data Privacy</Text>
+              <MaterialIconButton
+                name="info"
+                iconProps={{color: 'action.active'}}
+              />
+            </HStack>
+          }
+          options={{
+            public: {text: t('projects.add.public')},
+            private: {text: t('projects.add.private')},
+          }}
+          blockName="data-privacy"
+        />
+      </VStack>
+      <SaveFAB title={t('general.save')} onPress={console.debug} />
+    </Box>
+  );
+}
