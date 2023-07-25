1fd1b5b8f751eca3c02f135de99eb38d64d76adf
Jeancarlo <jeancarlobarrios@gmail.com>
2023-07-25 19:02:33 +0200

    feat: add outh redirects to ios

--- dev-client/src/auth/index.ts
+++ dev-client/src/auth/index.ts
@@ -3,12 +3,13 @@ import {APP_CONFIG} from '../config';
 import {OAuthProvider} from '../types';
 import {request} from 'terraso-client-shared/terrasoApi/api';
 import {getAPIConfig} from 'terraso-client-shared/config';
+import {Platform} from "react-native";
 
 // https://github.com/FormidableLabs/react-native-app-auth/blob/main/docs/config-examples/google.md
 const googleConfig = {
   issuer: 'https://accounts.google.com',
   clientId: APP_CONFIG.googleClientId,
-  redirectUrl: `${APP_CONFIG.packageName}:/oauth2redirect`,
+  redirectUrl: APP_CONFIG.googleRedirectURI,
   scopes: ['openid', 'profile', 'email'],
 };
 
@@ -37,7 +38,10 @@ const apiConfig = getAPIConfig();
 
 export async function auth() {
   let result = await authorize(googleConfig);
-  let {atoken, rtoken} = await exchangeToken(result.idToken, 'google');
+  let {atoken, rtoken} = await exchangeToken(
+    result.idToken,
+    Platform.OS === 'android' ? 'google-android' : 'google-ios',
+  );
   return Promise.all([
     apiConfig.tokenStorage.setToken('atoken', atoken),
     apiConfig.tokenStorage.setToken('rtoken', rtoken),
