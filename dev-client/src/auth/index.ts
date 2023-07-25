a37fac5e7e1285e2e7a2fdbab36e61e3791a35f1
Jeancarlo <jeancarlobarrios@gmail.com>
2023-07-25 20:58:48 +0200

    feat: add outh redirects to ios

--- dev-client/src/auth/index.ts
+++ dev-client/src/auth/index.ts
@@ -3,7 +3,7 @@ import {APP_CONFIG} from '../config';
 import {OAuthProvider} from '../types';
 import {request} from 'terraso-client-shared/terrasoApi/api';
 import {getAPIConfig} from 'terraso-client-shared/config';
-import {Platform} from "react-native";
+import {Platform} from 'react-native';
 
 // https://github.com/FormidableLabs/react-native-app-auth/blob/main/docs/config-examples/google.md
 const googleConfig = {
@@ -22,11 +22,15 @@ export async function exchangeToken(
   identityJwt: string,
   provider: OAuthProvider,
 ) {
+  console.log("EXCHANEE!!!");
+  console.log(provider);
   const payload = await request<AuthTokens>({
     path: '/auth/token-exchange',
     body: {provider, jwt: identityJwt},
     headers: {'content-type': 'application/json'},
   });
+  console.log("asdasdas");
+  console.log(payload);
 
   return {
     atoken: payload.atoken,
@@ -35,6 +39,8 @@ export async function exchangeToken(
 }
 
 const apiConfig = getAPIConfig();
+
+console.log(apiConfig);
 
 export async function auth() {
   let result = await authorize(googleConfig);
