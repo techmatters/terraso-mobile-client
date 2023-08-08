7c22217e7357ffb91315242181674f4d37ce454e
David Code Howard <david@techmatters.org>
2023-08-08 12:56:26 -0400

    fix: Fix up config variable naming, google redirect uri

--- dev-client/src/constants.ts
+++ dev-client/src/constants.ts
@@ -1,4 +1,5 @@
 /** Minimum distance to travel before Mapbox will update user location */
+export const PACKAGE_NAME = 'org.terraso.landpks';
 export const USER_DISPLACEMENT_MIN_DISTANCE_M = 1;
 export const SITE_NAME_MIN = 3;
 export const SITE_NAME_MAX = 120;
