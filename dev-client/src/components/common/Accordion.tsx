6192c1dd12eae80df36a3754922e6fdbb89ce1c6
shrouxm <mushrouxm@gmail.com>
2023-08-15 14:25:37 -0700

    feat: update location dashboard style, enable for non-sites

--- dev-client/src/components/common/Accordion.tsx
+++ dev-client/src/components/common/Accordion.tsx
@@ -4,30 +4,31 @@ import {IconButton} from './Icons';
 
 type Props = {
   Head: ReactNode;
-  Body: ReactNode;
+  children: ReactNode;
 };
 
-export function Accordion({Head, Body}: Props) {
+export function Accordion({Head, children}: Props) {
   const [open, setOpen] = useState(false);
   const onPress = useCallback(() => {
     setOpen(!open);
   }, [open, setOpen]);
 
   const name = open ? 'expand-less' : 'expand-more';
-  const icon = (
-    <IconButton
-      name={name}
-      onPress={onPress}
-      _icon={{color: 'action.active'}}
-    />
-  );
   return (
     <Box>
-      <HStack>
-        <Box>{Head}</Box>
-        {icon}
+      <HStack
+        backgroundColor="primary.main"
+        alignItems="center"
+        justifyContent="space-between"
+        px="16px">
+        {Head}
+        <IconButton
+          name={name}
+          onPress={onPress}
+          _icon={{color: 'primary.contrast'}}
+        />
       </HStack>
-      {open && <Box ml={3}>{Body}</Box>}
+      {open && children}
     </Box>
   );
 }
