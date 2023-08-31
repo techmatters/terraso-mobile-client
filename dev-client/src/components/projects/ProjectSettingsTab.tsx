0fa0158feee83713c494dd0c111190a06e623888
shrouxm <mushrouxm@gmail.com>
2023-08-31 11:49:42 -0700

    fix: project form (FAB placement, form validation msgs)

--- dev-client/src/components/projects/ProjectSettingsTab.tsx
+++ dev-client/src/components/projects/ProjectSettingsTab.tsx
@@ -1,9 +1,9 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {AlertDialog, Button, VStack, ScrollView} from 'native-base';
+import {AlertDialog, Button, VStack, ScrollView, Fab} from 'native-base';
 import {TabRoutes, TabStackParamList} from './constants';
 import {useTranslation} from 'react-i18next';
 import IconLink from '../common/IconLink';
-import {useRef, useState} from 'react';
+import {useMemo, useRef, useState} from 'react';
 import {useDispatch} from '../../model/store';
 import {
   deleteProject,
@@ -11,7 +11,11 @@ import {
   archiveProject,
 } from 'terraso-client-shared/project/projectSlice';
 import {useNavigation} from '../../screens/AppScaffold';
-import ProjectSettingsForm, {FormValues} from './CreateProjectView/Form';
+import ProjectSettingsForm, {
+  ProjectFormValues,
+  projectValidationSchema,
+} from './CreateProjectView/Form';
+import {Formik} from 'formik';
 
 type Props = NativeStackScreenProps<TabStackParamList, TabRoutes.SETTINGS>;
 
@@ -24,10 +28,11 @@ export default function ProjectSettingsTab({
   const dispatch = useDispatch();
 
   const formInitialValues = {name, description, privacy};
-
-  const onSubmit = async (values: FormValues) => {
+  const onSubmit = async (values: ProjectFormValues) => {
     await dispatch(updateProject({...values, id: projectId}));
   };
+  const validationSchema = useMemo(() => projectValidationSchema(t), [t]);
+
   const navigation = useNavigation();
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const cancelRef = useRef(null);
@@ -55,86 +60,101 @@ export default function ProjectSettingsTab({
     navigation.navigate('PROJECT_LIST');
   };
   return (
-    <ScrollView>
-      <VStack px={2} py={4} space={1} m={3} height="100%">
-        <ProjectSettingsForm
-          onSubmit={onSubmit}
-          initialValues={formInitialValues}
-          editForm={true}
-        />
-        <IconLink
-          iconName="content-copy"
-          underlined={false}
-          href={downloadLink}>
-          {t('projects.settings.copy_download_link').toUpperCase()}
-        </IconLink>
-        <IconLink
-          iconName="archive"
-          underlined={false}
-          onPress={openArchiveProject}>
-          {t('projects.settings.archive').toUpperCase()}
-        </IconLink>
-        <AlertDialog
-          leastDestructiveRef={cancelRef}
-          isOpen={isArchiveModalOpen}
-          onClose={closeArchiveProject}>
-          <AlertDialog.Content>
-            <AlertDialog.CloseButton />
-            <AlertDialog.Header>
-              {t('projects.settings.archive_button_prompt')}
-            </AlertDialog.Header>
-            <AlertDialog.Body>
-              {t('projects.settings.archive_description')}
-            </AlertDialog.Body>
-            <AlertDialog.Footer>
-              <Button.Group space={2}>
-                <Button
-                  variant="unstyled"
-                  colorScheme="coolGray"
-                  onPress={closeArchiveProject}>
-                  {t('projects.settings.cancel')}
-                </Button>
-                <Button colorScheme="danger" onPress={triggerArchiveProject}>
-                  {t('projects.settings.archive_button')}
-                </Button>
-              </Button.Group>
-            </AlertDialog.Footer>
-          </AlertDialog.Content>
-        </AlertDialog>
-        <IconLink
-          iconName="delete-forever"
-          underlined={false}
-          onPress={openDeleteProject}>
-          {t('projects.settings.delete').toUpperCase()}
-        </IconLink>
-        <AlertDialog
-          leastDestructiveRef={cancelRef}
-          isOpen={isDeleteModalOpen}
-          onClose={closeDeleteProject}>
-          <AlertDialog.Content>
-            <AlertDialog.CloseButton />
-            <AlertDialog.Header>
-              {t('projects.settings.delete_button_prompt')}
-            </AlertDialog.Header>
-            <AlertDialog.Body>
-              {t('projects.settings.delete_description')}
-            </AlertDialog.Body>
-            <AlertDialog.Footer>
-              <Button.Group space={2}>
-                <Button
-                  variant="unstyled"
-                  colorScheme="coolGray"
-                  onPress={closeDeleteProject}>
-                  {t('projects.settings.cancel')}
-                </Button>
-                <Button colorScheme="danger" onPress={triggerDeleteProject}>
-                  {t('projects.settings.delete_button')}
-                </Button>
-              </Button.Group>
-            </AlertDialog.Footer>
-          </AlertDialog.Content>
-        </AlertDialog>
-      </VStack>
-    </ScrollView>
+    <Formik
+      initialValues={formInitialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}>
+      {({handleSubmit, isSubmitting}) => (
+        <>
+          <ScrollView>
+            <VStack px={2} py={4} space={1} m={3} height="100%">
+              <ProjectSettingsForm editForm={true} />
+              <IconLink
+                iconName="content-copy"
+                underlined={false}
+                href={downloadLink}>
+                {t('projects.settings.copy_download_link').toUpperCase()}
+              </IconLink>
+              <IconLink
+                iconName="archive"
+                underlined={false}
+                onPress={openArchiveProject}>
+                {t('projects.settings.archive').toUpperCase()}
+              </IconLink>
+              <AlertDialog
+                leastDestructiveRef={cancelRef}
+                isOpen={isArchiveModalOpen}
+                onClose={closeArchiveProject}>
+                <AlertDialog.Content>
+                  <AlertDialog.CloseButton />
+                  <AlertDialog.Header>
+                    {t('projects.settings.archive_button_prompt')}
+                  </AlertDialog.Header>
+                  <AlertDialog.Body>
+                    {t('projects.settings.archive_description')}
+                  </AlertDialog.Body>
+                  <AlertDialog.Footer>
+                    <Button.Group space={2}>
+                      <Button
+                        variant="unstyled"
+                        colorScheme="coolGray"
+                        onPress={closeArchiveProject}>
+                        {t('projects.settings.cancel')}
+                      </Button>
+                      <Button
+                        colorScheme="danger"
+                        onPress={triggerArchiveProject}>
+                        {t('projects.settings.archive_button')}
+                      </Button>
+                    </Button.Group>
+                  </AlertDialog.Footer>
+                </AlertDialog.Content>
+              </AlertDialog>
+              <IconLink
+                iconName="delete-forever"
+                underlined={false}
+                onPress={openDeleteProject}>
+                {t('projects.settings.delete').toUpperCase()}
+              </IconLink>
+              <AlertDialog
+                leastDestructiveRef={cancelRef}
+                isOpen={isDeleteModalOpen}
+                onClose={closeDeleteProject}>
+                <AlertDialog.Content>
+                  <AlertDialog.CloseButton />
+                  <AlertDialog.Header>
+                    {t('projects.settings.delete_button_prompt')}
+                  </AlertDialog.Header>
+                  <AlertDialog.Body>
+                    {t('projects.settings.delete_description')}
+                  </AlertDialog.Body>
+                  <AlertDialog.Footer>
+                    <Button.Group space={2}>
+                      <Button
+                        variant="unstyled"
+                        colorScheme="coolGray"
+                        onPress={closeDeleteProject}>
+                        {t('projects.settings.cancel')}
+                      </Button>
+                      <Button
+                        colorScheme="danger"
+                        onPress={triggerDeleteProject}>
+                        {t('projects.settings.delete_button')}
+                      </Button>
+                    </Button.Group>
+                  </AlertDialog.Footer>
+                </AlertDialog.Content>
+              </AlertDialog>
+            </VStack>
+          </ScrollView>
+          <Fab
+            label={t('general.save_fab')}
+            onPress={() => handleSubmit()}
+            disabled={isSubmitting}
+            renderInPortal={false}
+          />
+        </>
+      )}
+    </Formik>
   );
 }
