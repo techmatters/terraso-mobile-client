596db2f1805ddecf97c79765d54a4245fd0a51c6
garo (they/them) <3411715+shrouxm@users.noreply.github.com>
2023-09-07 12:32:05 -0700

    feat: screens refactor with fix for home screen bottom sheet (#407)

--- dev-client/src/screens/ScreenScaffold.tsx
+++ dev-client/src/screens/ScreenScaffold.tsx
@@ -1,16 +1,67 @@
-import {Box, Flex, HStack} from 'native-base';
+import {Box, Row, Heading, Column} from 'native-base';
 import {
   IconButton,
   IconButtonProps,
   MaterialCommunityIcons,
 } from '../components/common/Icons';
-import {useNavigation} from './AppScaffold';
-import {useCallback} from 'react';
+import {createContext, useCallback, useContext, useState} from 'react';
 import {useTranslation} from 'react-i18next';
+import {useRoute} from '@react-navigation/native';
+import {useNavigation} from './AppScaffold';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {StatusBar, View, LayoutChangeEvent} from 'react-native';
+
+const HeaderHeightContext = createContext<number | undefined>(undefined);
+export const useHeaderHeight = () => useContext(HeaderHeightContext);
 
 const BottomNavIconButton = (props: IconButtonProps & {label: string}) => (
   <IconButton pb={0} _icon={{color: 'primary.contrast'}} {...props} />
 );
+
+export const AppBarIconButton = (props: IconButtonProps) => (
+  <IconButton size="md" _icon={{color: 'primary.contrast'}} {...props} />
+);
+
+export const ScreenBackButton = ({icon = 'arrow-back'}: {icon?: string}) => {
+  const navigation = useNavigation();
+  const goBack = useCallback(() => navigation.pop(), [navigation]);
+  return <AppBarIconButton name={icon} onPress={goBack} />;
+};
+
+export const ScreenCloseButton = () => <ScreenBackButton icon="close" />;
+
+type AppBarProps = {
+  LeftButton?: React.ReactNode;
+  RightButton?: React.ReactNode;
+  title?: string;
+};
+
+export const AppBar = ({
+  LeftButton = <ScreenBackButton />,
+  RightButton,
+  title,
+}: AppBarProps) => {
+  const {t} = useTranslation();
+  const route = useRoute();
+  const safeAreaTopInset = useSafeAreaInsets().top;
+
+  return (
+    <Row
+      px="8px"
+      py="4px"
+      pt={`${safeAreaTopInset}px`}
+      minHeight="56px"
+      bg="primary.main">
+      <Row flex={1} space="24px" alignItems="center">
+        {LeftButton}
+        <Heading variant="h6" color="primary.contrast">
+          {title ?? t(`screens.${route.name}`)}
+        </Heading>
+      </Row>
+      {RightButton}
+    </Row>
+  );
+};
 
 export const BottomNavigation = () => {
   const {t} = useTranslation();
@@ -24,7 +75,7 @@ export const BottomNavigation = () => {
   );
 
   return (
-    <HStack bg="primary.main" justifyContent="center" space={10} pb={2}>
+    <Row bg="primary.main" justifyContent="center" space={10} pb={2}>
       <BottomNavIconButton
         name="location-pin"
         label={t('bottom_navigation.home')}
@@ -40,17 +91,43 @@ export const BottomNavigation = () => {
         name="settings"
         label={t('bottom_navigation.settings')}
       />
-    </HStack>
+    </Row>
   );
 };
 
-type Props = React.ComponentProps<typeof Box>;
+type Props = {
+  children: React.ReactNode;
+  AppBar?: React.ReactNode;
+  BottomNavigation?: React.ReactNode;
+};
+export const ScreenScaffold = ({
+  children,
+  AppBar: PropsAppBar = <AppBar />,
+  BottomNavigation: PropsBottomNavigation = <BottomNavigation />,
+}: Props) => {
+  const [headerHeight, setHeaderHeight] = useState<number | undefined>(
+    undefined,
+  );
 
-export const ScreenScaffold = ({children, ...props}: Props) => (
-  <Flex flex={1}>
-    <Box flex={1} {...props}>
-      {children}
-    </Box>
-    <BottomNavigation />
-  </Flex>
-);
+  const onLayout = useCallback(
+    (e: LayoutChangeEvent) => setHeaderHeight(e.nativeEvent.layout.height),
+    [setHeaderHeight],
+  );
+
+  return (
+    <>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor={'#00000000'}
+      />
+      <Column flex={1}>
+        <View onLayout={onLayout}>{PropsAppBar}</View>
+        <HeaderHeightContext.Provider value={headerHeight ?? 0}>
+          <Box flex={1}>{children}</Box>
+        </HeaderHeightContext.Provider>
+        {PropsBottomNavigation}
+      </Column>
+    </>
+  );
+};
