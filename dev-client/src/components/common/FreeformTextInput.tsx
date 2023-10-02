2abc61986182918f074f540f6d5872b42d72e390
David Code Howard <david@techmatters.org>
2023-10-02 15:50:30 -0400

    feat: Add free form text input
    This input is intended for the add user to project screen. It will
    just encapsulate the logic of validating the user emails exists and
    displaying an error message if invalid. In the future the error
    message may be more complex, like displaying a modal or something, so
    I've decided to make this a whole separate component.

---
+++ dev-client/src/components/common/FreeformTextInput.tsx
@@ -0,0 +1,43 @@
+import {FormControl, Input} from 'native-base';
+import {useCallback, useState} from 'react';
+
+type Props = {
+  validationFunc: (input: string) => Promise<null | string>;
+  helperText?: string;
+  placeholder?: string;
+};
+
+/**
+ * A text input that allows the user to directly enter a text string.
+ * On submit, the result is validated. If incorrect, an error message is
+ * displayed.
+ */
+export const FreeformTextInput = ({
+  validationFunc,
+  helperText,
+  placeholder,
+}: Props) => {
+  const [hasError, setHasError] = useState<null | string>(null);
+  const [textValue, setTextValue] = useState<string>('');
+
+  const handleSubmit = useCallback(async () => {
+    const validationResults = await validationFunc(textValue);
+    if (validationResults !== null) {
+      setHasError(validationResults);
+    } else {
+      setTextValue('');
+    }
+  }, [validationFunc, textValue, setTextValue]);
+
+  <FormControl>
+    <Input
+      placeholder={placeholder !== undefined ? placeholder : ''}
+      onSubmitEditing={handleSubmit}
+      onChangeText={text => setTextValue(text)}
+      value={textValue}></Input>
+    {helperText !== '' ? (
+      <FormControl.HelperText>{{helperText}}</FormControl.HelperText>
+    ) : null}
+    <FormControl.ErrorMessage>{{hasError}}</FormControl.ErrorMessage>
+  </FormControl>;
+};
