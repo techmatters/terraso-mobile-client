7f9bb29f65ac380dc5324b1b8029010a7e487dfe
David Code Howard <david@techmatters.org>
2023-10-02 16:05:26 -0400

    chore: Save (add user to project)

---
+++ dev-client/src/components/projects/AddUserToProjectScreen.tsx
@@ -0,0 +1,7 @@
+import {searchUser} from 'terraso-client-shared/account/accountSlice';
+
+type Props = {
+  projectId: string;
+};
+
+export const AddUserToProjectScreen = ({projectId}: Props) => {};
