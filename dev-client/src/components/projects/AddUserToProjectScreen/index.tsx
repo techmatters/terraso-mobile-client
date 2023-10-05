44ee63aa03c48cbcf7abccf65d541af6665122b9
David Code Howard <david@techmatters.org>
2023-10-05 14:52:53 -0400

    feat: Add logic to handle submit

--- dev-client/src/components/projects/AddUserToProjectScreen/index.tsx
+++ dev-client/src/components/projects/AddUserToProjectScreen/index.tsx
@@ -1,4 +1,4 @@
-import {Box, ScrollView} from 'native-base';
+import {Fab} from 'native-base';
 import {FreeformTextInput} from '../../common/FreeformTextInput';
 import {useTranslation} from 'react-i18next';
 import {ScreenScaffold} from '../../../screens/ScreenScaffold';
@@ -6,6 +6,9 @@ import {useCallback, useMemo, useState} from 'react';
 import {UserRole} from 'terraso-client-shared/graphqlSchema/graphql';
 import {checkUserInProject} from 'terraso-client-shared/account/accountService';
 import MembershipControlList, {UserWithRole} from './MembershipControlList';
+import {addUserToProject} from 'terraso-client-shared/project/projectSlice';
+import {useNavigation} from '../../../screens/AppScaffold';
+import {useDispatch} from '../../../model/store';
 
 type Props = {
   projectId: string;
@@ -16,6 +19,10 @@ export const AddUserToProjectScreen = ({projectId}: Props) => {
   const [userRecord, setUserRecord] = useState<Record<string, UserWithRole>>(
     {},
   );
+
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
 
   const userList = useMemo(() => Object.values(userRecord), [userRecord]);
 
@@ -57,6 +64,22 @@ export const AddUserToProjectScreen = ({projectId}: Props) => {
     });
   }, []);
 
+  const submitUsers = async () => {
+    setIsSubmitting(true);
+    for (const {
+      user: {id: userId},
+      role,
+    } of Object.values(userRecord)) {
+      try {
+        dispatch(addUserToProject({userId, role, projectId}));
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    navigation.pop();
+    setIsSubmitting(false);
+  };
+
   return (
     <ScreenScaffold>
       <FreeformTextInput
@@ -67,6 +90,12 @@ export const AddUserToProjectScreen = ({projectId}: Props) => {
         users={userList}
         updateUserRole={updateUserRole}
         removeUser={removeUser}
+      />
+      <Fab
+        label={t('general.save_fab')}
+        onPress={submitUsers}
+        disabled={isSubmitting}
+        renderInPortal={false}
       />
     </ScreenScaffold>
   );
