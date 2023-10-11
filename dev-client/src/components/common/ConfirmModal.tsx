fe5ed8f8df4e44380668a7190a5ddfeb41325733
David Code Howard <david@techmatters.org>
2023-10-11 15:55:58 -0400

    chore: save

---
+++ dev-client/src/components/common/ConfirmModal.tsx
@@ -0,0 +1,12 @@
+type Props = {
+  title: string;
+  bodyParagraphs: string[];
+  actionName: string;
+};
+
+/**
+ * Modal presented to a user when asked to confirm a decision
+ */
+const ConfirmModal = ({title, bodyParagraphs, actionName}: Props) => {
+  return <Modal></Modal>;
+};
