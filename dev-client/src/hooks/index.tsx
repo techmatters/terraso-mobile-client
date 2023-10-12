cd96434bde402715fa7ac241e6dcfc9614f7dfe1
David Code Howard <david@techmatters.org>
2023-10-12 10:14:19 -0400

    feat: Add hook for when keyboard is open (#479)
    * feat: Add hook for when keyboard is open
    
    * fix: Add empty dependency array

---
+++ dev-client/src/hooks/index.tsx
@@ -0,0 +1,22 @@
+import {useEffect, useState} from 'react';
+import {Keyboard} from 'react-native';
+
+export const useKeyboardOpen = () => {
+  const [keyboardOpen, setKeyboardOpen] = useState<boolean>(
+    Keyboard.isVisible(),
+  );
+  useEffect(() => {
+    const openListener = Keyboard.addListener('keyboardDidShow', () =>
+      setKeyboardOpen(true),
+    );
+    const closeListener = Keyboard.addListener('keyboardDidHide', () =>
+      setKeyboardOpen(false),
+    );
+    return () => {
+      openListener.remove();
+      closeListener.remove();
+    };
+  }, []);
+
+  return keyboardOpen;
+};
