20cad7d5bee19690fa3512b8a896c6f3343f5661
Paul Schreiber <paul@techmatters.org>
2023-10-20 14:44:30 -0400

    fix: adjust keyboard settings for email address

--- dev-client/src/components/projects/AddUserToProjectScreen/index.tsx
+++ dev-client/src/components/projects/AddUserToProjectScreen/index.tsx
@@ -113,6 +113,9 @@ export const AddUserToProjectScreen = ({projectId}: Props) => {
         <FreeformTextInput
           validationFunc={validationFunc}
           placeholder={t('general.example_email')}
+          autoComplete="email"
+          autoCapitalize="none"
+          keyboardType="email-address"
         />
       </Box>
       <MembershipControlList
