a9770f4e5798a8742e55837deb6236999e56568b
Paul Schreiber <paul@techmatters.org>
2023-10-26 16:54:45 -0400

    fix: add RCTLinkingManager to AppDelegate.h

--- dev-client/ios/Terraso LandPKS/AppDelegate.h
+++ dev-client/ios/Terraso LandPKS/AppDelegate.h
@@ -1,6 +1,6 @@
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
-
+#import <React/RCTLinkingManager.h>
 #import "RNAppAuthAuthorizationFlowManager.h"
 
 @interface AppDelegate : RCTAppDelegate <UIApplicationDelegate, RCTBridgeDelegate, RNAppAuthAuthorizationFlowManager>
