6cf22114c33539a09bbfa8141a92400722262227
David Code Howard <david@techmatters.org>
2023-10-30 12:10:09 -0400

    feat: Site Transfer (#549)
    * build: Bump client-shared
    
    * feat: Add Checkbox Group
    
    * feat: Add proper logic to site transfer
    
    * feat: Dispatch transfer mutation
    
    * fix: Render checkboxes in FlatList
    
    * fix: Don't display sites of empty projects
    
    * fix: Do some sorting
    
    * feat: Navigate back after transfer
    
    * fix: Styling changes
    
    * feat: Add unaffiliated sites to site transfer
    
    * fix: Fix unaffiliated search, allow scrolling
    
    * fix: Allow transferring unaffiliated sites
    
    * fix(lint): Fix linting errors

--- dev-client/src/util.ts
+++ dev-client/src/util.ts
@@ -1,3 +1,27 @@
-export function formatName(firstName: string, lastName?: string) {
+export const formatName = (firstName: string, lastName?: string) => {
   return [lastName, firstName].filter(Boolean).join(', ');
-}
+};
+
+export const removeKeys = (a: any, b: any) => {
+  const remove = [a, b];
+  let currA, currB;
+  while (remove.length) {
+    currA = remove.pop();
+    currB = remove.pop();
+    for (const keyA of Object.keys(currA)) {
+      if (!(keyA in currB)) {
+        delete currA[keyA];
+        continue;
+      }
+      const valA = currA[keyA];
+      const valB = currB[keyA];
+      if (typeof valA !== typeof valB) {
+        delete currA[keyA];
+        continue;
+      }
+      if (typeof valA === 'object' && !Array.isArray(valA) && valA !== null) {
+        remove.push(valA, valB);
+      }
+    }
+  }
+};
