4f8869476021bc95b5685d63437b94d595389de4
Paul Schreiber <paul@techmatters.org>
2023-11-03 12:10:21 -0400

    feat: Add Microsoft and Apple login support (#559)
    * Add Microsoft and Apple login support — buttons, OpenID configuration
    * match login button text keys to web app
    * temporarily disable Apple logins (web login doesn't work due to lack of request_mode: form_post support)

--- dev-client/src/auth/index.ts
+++ dev-client/src/auth/index.ts
@@ -13,6 +13,33 @@ const googleConfig = {
   scopes: ['openid', 'profile', 'email'],
 };
 
+const appleConfig = {
+  issuer: 'https://appleid.apple.com',
+  clientId: APP_CONFIG.appleClientId,
+  redirectUrl: APP_CONFIG.appleRedirectURI,
+  scopes: ['name', 'email'],
+};
+
+/*
+  Using issuer fails, because MS returns issuer: "https://login.microsoftonline.com/{tenantid}/v2.0"
+ from https://login.microsoftonline.com/common//v2.0/.well-known/openid-configuration.
+
+  React Native App Auth trys to load "issuer", resulting in a 404 error and the JSON parsing fails with this error:
+  Error: JSON error parsing document at 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration': Invalid URL: issuer
+
+  Workaround is to pass serviceConfiguration directly. Note that Microsoft does ot have a revocationEndpoint.
+ */
+const microsoftConfig = {
+  serviceConfiguration: {
+    authorizationEndpoint:
+      'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
+    tokenEndpoint: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+  },
+  clientId: APP_CONFIG.microsoftClientId,
+  redirectUrl: APP_CONFIG.microsoftRedirectURI,
+  scopes: ['openid', 'profile', 'email', 'offline_access'],
+};
+
 interface AuthTokens {
   atoken: string;
   rtoken: string;
@@ -36,13 +63,22 @@ export async function exchangeToken(
 
 const apiConfig = getAPIConfig();
 
-export async function auth() {
-  let result = await authorize(googleConfig);
-  var platformOs = '';
-  if (Platform.OS === 'android') {
-    platformOs = 'google-android';
-  } else if (Platform.OS === 'ios') {
-    platformOs = 'google-ios';
+export async function auth(providerName: String) {
+  let result;
+  let platformOs = providerName;
+  if (providerName === 'google') {
+    result = await authorize(googleConfig);
+    if (Platform.OS === 'android') {
+      platformOs = 'google-android';
+    } else if (Platform.OS === 'ios') {
+      platformOs = 'google-ios';
+    }
+  } else if (providerName === 'apple') {
+    result = await authorize(appleConfig);
+  } else if (providerName === 'microsoft') {
+    result = await authorize(microsoftConfig);
+  } else {
+    throw new Error(`${providerName} is not a recognized OAuth provider.`);
   }
   let {atoken, rtoken} = await exchangeToken(
     result.idToken,
