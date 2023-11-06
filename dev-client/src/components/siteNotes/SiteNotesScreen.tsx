ffb17dd764df13e2ff5bf5adf9f8119cf8a4fb50
Aja Beckett <ajabeckett@gmail.com>
2023-11-06 12:14:56 -0800

    feat: style delete button
    IconButton label should probably be horizontal, if present

--- dev-client/src/components/siteNotes/SiteNotesScreen.tsx
+++ dev-client/src/components/siteNotes/SiteNotesScreen.tsx
@@ -1,35 +1,18 @@
-import {useState} from 'react';
 import {
   Box,
   Button,
   Column,
   Heading,
   Row,
-  Text,
-  VStack,
-  HStack,
-  Spacer,
   FlatList,
 } from 'native-base';
 import {useDispatch, useSelector} from 'terraso-mobile-client/model/store';
 import {useTranslation} from 'react-i18next';
-import {
-  BottomSheetModal,
-  Modal,
-  useModal,
-} from 'terraso-mobile-client/components/common/Modal';
-import {deleteSiteNote} from 'terraso-client-shared/site/siteSlice';
-import {useCallback} from 'react';
+import {Modal} from 'terraso-mobile-client/components/common/Modal';
 import SiteNote from 'terraso-client-shared/site/siteSlice';
-import {Card} from 'terraso-mobile-client/components/common/Card';
-import {formatDate} from 'terraso-mobile-client/util';
-import {IconButton} from 'terraso-mobile-client/components/common/Icons';
 import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
-import * as yup from 'yup';
-import {Formik} from 'formik';
-import {FormInput} from 'terraso-mobile-client/components/common/Form';
 import {AddSiteNoteModal} from 'terraso-mobile-client/components/siteNotes/AddSiteNoteModal';
-import {EditSiteNoteModal} from 'terraso-mobile-client/components/siteNotes/EditSiteNoteModal';
+import {SiteNoteCard} from 'terraso-mobile-client/components/siteNotes/SiteNoteCard';
 
 export type SiteNote = {
   id: string;
@@ -42,7 +25,6 @@ export type SiteNote = {
 export const SiteNotesScreen = ({siteId}: {siteId: string}) => {
   const {t} = useTranslation();
   const notes = useSelector(state => state.site.sites[siteId].notes);
-  const dispatch = useDispatch();
 
   return (
     <BottomSheetModalProvider>
@@ -70,41 +52,7 @@ export const SiteNotesScreen = ({siteId}: {siteId: string}) => {
           mb={130}
           data={Object.values(notes)}
           keyExtractor={note => note.id}
-          renderItem={({item: note}) => (
-            <Card
-              key={note.id}
-              alignItems="flex-start"
-              shadow={0}
-              mb={3}
-              ml={4}
-              mr={4}>
-              <HStack>
-                <Text italic>
-                  {formatDate(note.createdAt)} by {note.authorFirstName}{' '}
-                  {note.authorLastName}
-                </Text>
-                <Spacer />
-                <Modal
-                  key={siteId}
-                  trigger={onOpen => (
-                    <IconButton
-                    p={0}
-                    name="edit"
-                    _icon={{
-                      color: 'grey.600',
-                      size: '5',
-                    }}
-                    onPress={onOpen}
-                  />
-                  )}>
-                  <EditSiteNoteModal note={note} />
-                </Modal>
-              </HStack>
-              <Text pt={1} fontSize="md">
-                {note.content}
-              </Text>
-            </Card>
-          )}
+          renderItem={({item: note}) => <SiteNoteCard siteId={siteId} note={note} />}
         />
       </Column>
     </BottomSheetModalProvider>
