f278b4784539d1d443985d6b6fadc16ecc1b4b35
Aja Beckett <ajabeckett@gmail.com>
2023-11-10 13:37:28 -0800

    feat: truncate notes and tap to open

--- dev-client/src/components/siteNotes/SiteInstructionsCard.tsx
+++ dev-client/src/components/siteNotes/SiteInstructionsCard.tsx
@@ -1,7 +1,9 @@
+import {useCallback} from 'react';
 import {Text, HStack} from 'native-base';
 import {useTranslation} from 'react-i18next';
 import {Card} from 'terraso-mobile-client/components/common/Card';
 import {Icon} from 'terraso-mobile-client/components/common/Icons';
+import {useNavigation} from 'terraso-mobile-client/screens/AppScaffold';
 
 type Props = {
   siteInstructions: string;
@@ -9,12 +11,23 @@ type Props = {
 
 export const SiteInstructionsCard = ({siteInstructions}: Props) => {
   const {t} = useTranslation();
+  const navigation = useNavigation();
+
+  const onShowNote = useCallback(() => {
+    navigation.navigate('READ_NOTE', {content: siteInstructions});
+  }, [navigation, siteInstructions]);
 
   return (
-    <Card alignItems="flex-start" shadow={4} mb={4} ml={4} mr={4}>
+    <Card
+      alignItems="flex-start"
+      shadow={4}
+      mb={4}
+      ml={4}
+      mr={4}
+      onPress={onShowNote}>
       <HStack>
-       <Icon name="place" color="primary.dark" size="sm" mr="1" />
-        <Text bold fontSize="md">
+        <Icon name="place" color="primary.dark" size="sm" mr="1" />
+        <Text bold fontSize="md" numberOfLines={3} ellipsizeMode="tail">
           {t('site.notes.projectInstructions')}
         </Text>
       </HStack>
