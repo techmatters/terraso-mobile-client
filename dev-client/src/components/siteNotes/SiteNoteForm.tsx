8b194f01db5db1074d0e6aa966e231c0a4105cf7
Aja Beckett <ajabeckett@gmail.com>
2023-11-10 18:01:23 -0800

    feat: make keyboard avoid work on iOS

--- dev-client/src/components/siteNotes/SiteNoteForm.tsx
+++ dev-client/src/components/siteNotes/SiteNoteForm.tsx
@@ -37,7 +37,7 @@ export const SiteNoteForm = ({
       onChangeText={onChangeContent}
       onBlur={onBlurContent}
       multiline
-      maxHeight={320}
+      maxHeight={200}
       overflow={'scroll'}
       textAlignVertical="top"
     />
