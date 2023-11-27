dd4f79fc948dc8fc75a670cfe3b8837768f6211e
David Code Howard <david@techmatters.org>
2023-11-27 11:46:50 -0500

    chore: Add copyright headers

--- dev-client/src/components/projects/ProjectList.tsx
+++ dev-client/src/components/projects/ProjectList.tsx
@@ -1,3 +1,19 @@
+/*
+ * Copyright © ${YEAR} Technology Matters
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
 import {Box, FlatList, Text} from 'native-base';
 import ProjectPreviewCard from 'terraso-mobile-client/components/projects/ProjectPreviewCard';
 import {useListFilter} from 'terraso-mobile-client/components/common/ListFilter';
