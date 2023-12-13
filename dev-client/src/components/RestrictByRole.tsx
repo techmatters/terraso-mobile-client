42c49e9ecbb8741cb55b8c2ea1b4c43323bed8fc
David Code Howard <david@techmatters.org>
2023-12-13 15:24:19 -0500

    feat: user role context (#682)
    * chore: Update client shared
    
    * feat: Add way to restrict on site role
    
    This commit adds a simple way to restrict when a given component is
    displayed. The `RestrictBySiteRole`component only displays its
    children if the user has the correct role in relation to the site.

---
+++ dev-client/src/components/RestrictByRole.tsx
@@ -0,0 +1,44 @@
+/*
+ * Copyright © 2023 Technology Matters
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+import {useMemo} from 'react';
+import {SiteUserRole} from 'terraso-client-shared/selectors';
+import {useSiteRoleContext} from 'terraso-mobile-client/context/SiteRoleContext';
+
+type RestrictSiteRoleProps = {
+  role: SiteUserRole | SiteUserRole[];
+} & React.PropsWithChildren;
+
+/** Only display child components with specified site role */
+export const RestrictBySiteRole = ({role, children}: RestrictSiteRoleProps) => {
+  const siteRole = useSiteRoleContext();
+  const display = useMemo(() => {
+    if (siteRole === null) {
+      return false;
+    }
+    if (role instanceof Array) {
+      return (
+        role.filter(
+          ({kind, role: userRole}) =>
+            siteRole.kind === kind && siteRole.role === userRole,
+        ).length > 0
+      );
+    }
+    return role === siteRole;
+  }, [siteRole, role]);
+
+  return display ? {children} : <></>;
+};
---
+++ dev-client/src/context/SiteRoleContext.tsx
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2023 Technology Matters
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+import {createContext, useContext} from 'react';
+import {
+  SiteUserRole,
+  selectUserRoleSite,
+} from 'terraso-client-shared/selectors';
+import {useSelector} from 'terraso-mobile-client/store';
+
+const SiteRoleContext = createContext<SiteUserRole | null>(null);
+
+type Props = {
+  siteId: string;
+} & React.PropsWithChildren;
+
+export const useSiteRoleContext = () => {
+  return useContext(SiteRoleContext);
+};
+
+export const SiteRoleContextProvider = ({siteId, children}: Props) => {
+  const role = useSelector(state => selectUserRoleSite(state, siteId));
+
+  return (
+    <SiteRoleContext.Provider value={role}>{children}</SiteRoleContext.Provider>
+  );
+};
