34b168c642e6d0fd701e7fd0440eb71810dae28d
Paul Schreiber <paul@techmatters.org>
2023-12-28 11:49:36 -0500

    chore: update MainActivity.java -> MainActivity.kt

---
+++ dev-client/android/app/src/main/java/org/terraso/landpks/MainActivity.kt
@@ -0,0 +1,32 @@
+package org.terraso.landpks
+
+import android.os.Bundle
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+
+  // https://reactnavigation.org/docs/getting-started/#installing-dependencies-into-a-bare-react-native-project
+    // This change is required to avoid crashes related to View state being not persisted consistently across Activity restarts.
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "Terraso LandPKS"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+    DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
