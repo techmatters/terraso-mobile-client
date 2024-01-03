3aba91b89c290210977742ebec461511c28f7db4
David Code Howard <david@techmatters.org>
2024-01-03 13:53:15 -0500

    feat: Improve switches, display error if connection to backend fails (#678)
    * feat: Display error on switch request failure
    
    In order to avoid laggy radio switches, I've changed the update to
    that it is automatically applied to the redux store. However, this
    means that we absolutely need to let the user know if the request
    wasn't actually applied on the backend due to a connection error. This
    commit displays a toast at the top level if the requests fails.
    
    * fix: Update soil ID status type
    
    * fix: Linting fixes
    
    * feat: Hook applyToAll checkbox to actually work
    
    * chore: Add yet another copyright
    
    * feat: Use required project settings in interval editing
    
    * fix: Fix linting errors
    
    * Update dev-client/src/translations/en.json
    
    ---------
    
    Co-authored-by: Paul Schreiber <paul@techmatters.org>

---
+++ dev-client/src/screens/Toasts.tsx
@@ -0,0 +1,39 @@
+/*
+ * Copyright © 2023 Technology Matters
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+import {useToast} from 'native-base';
+import {useEffect} from 'react';
+import {useTranslation} from 'react-i18next';
+import {setSoilIdStatus} from 'terraso-client-shared/soilId/soilIdSlice';
+import {useDispatch, useSelector} from 'terraso-mobile-client/store';
+
+export const Toasts = () => {
+  const dispatch = useDispatch();
+  const {t} = useTranslation();
+  const toast = useToast();
+  const soilIdStatus = useSelector(state => state.soilId.status);
+
+  useEffect(() => {
+    if (soilIdStatus === 'error') {
+      toast.show({
+        description: t('errors.soilId'),
+      });
+      dispatch(setSoilIdStatus('ready'));
+    }
+  }, [soilIdStatus, dispatch, toast, t]);
+
+  return <></>;
+};
