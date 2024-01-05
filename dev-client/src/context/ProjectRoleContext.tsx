dd09cc7c627ad0391feee754db4fdfc221c2f268
David Code Howard <david@techmatters.org>
2024-01-05 13:13:41 -0500

    feat: project depth intervals (#742)
    * feat: Determine project soil depth intervals from preset
    
    * refactor: Split data table into separate component
    
    * feat: Restrict screen actions based on user role
    
    * refactor: Move components into different files
    
    * fix: Center DataGridTable text if only one column
    
    * fix: Fix linting issues
    
    * chore: Add copyrights, cosmetic fixups

---
+++ dev-client/src/context/ProjectRoleContext.tsx
@@ -0,0 +1,40 @@
+/*
+ * Copyright © 2024 Technology Matters
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+import {createContext, useContext} from 'react';
+import {UserRole} from 'terraso-client-shared/graphqlSchema/graphql';
+import {selectUserRoleProject} from 'terraso-client-shared/selectors';
+import {useSelector} from 'terraso-mobile-client/store';
+
+const ProjectRoleContext = createContext<UserRole | null>(null);
+
+type Props = {
+  projectId: string;
+} & React.PropsWithChildren;
+
+export const useProjectRoleContext = () => {
+  return useContext(ProjectRoleContext);
+};
+
+export const ProjectRoleContextProvider = ({projectId, children}: Props) => {
+  const role = useSelector(state => selectUserRoleProject(state, projectId));
+
+  return (
+    <ProjectRoleContext.Provider value={role}>
+      {children}
+    </ProjectRoleContext.Provider>
+  );
+};
---
+++ dev-client/src/screens/ProjectInputScreen/RequiredDataSettings.tsx
@@ -0,0 +1,68 @@
+/*
+ * Copyright © 2024 Technology Matters
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+import {Row, Text, Box, useTheme} from 'native-base';
+import {useTranslation} from 'react-i18next';
+import {useDispatch, useSelector} from 'terraso-mobile-client/store';
+import {
+  collectionMethods,
+  methodRequired,
+  updateProjectSoilSettings,
+} from 'terraso-client-shared/soilId/soilIdSlice';
+import {Switch} from 'react-native';
+
+export const RequiredDataSettings = ({projectId}: {projectId: string}) => {
+  const {t} = useTranslation();
+  const settings = useSelector(
+    state => state.soilId.projectSettings[projectId],
+  );
+  const dispatch = useDispatch();
+  const {colors} = useTheme();
+
+  return (
+    <Box p={4}>
+      {collectionMethods.map(method => (
+        <Row
+          key={method}
+          pt={2}
+          pb={5}
+          justifyContent="flex-start"
+          alignItems="center">
+          <Switch
+            value={settings[methodRequired(method)]}
+            thumbColor={
+              settings[methodRequired(method)]
+                ? colors.primary.main
+                : colors.primary.contrast
+            }
+            onValueChange={value => {
+              dispatch(
+                updateProjectSoilSettings({
+                  projectId,
+                  [methodRequired(method)]: value,
+                }),
+              );
+            }}
+          />
+          <Text bold pl={2} fontSize="md">
+            {t(`soil.collection_method.${method}`)}
+          </Text>
+        </Row>
+      ))}
+    </Box>
+  );
+};
