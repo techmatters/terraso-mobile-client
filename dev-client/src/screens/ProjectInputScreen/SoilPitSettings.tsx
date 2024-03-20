a3a2d120bd697d14a2075dc8cbe8d049a53dc11b
shrouxm <mushrouxm@gmail.com>
2024-03-20 15:45:18 -0700

    refactor: migrate some last files

--- dev-client/src/screens/ProjectInputScreen/SoilPitSettings.tsx
+++ dev-client/src/screens/ProjectInputScreen/SoilPitSettings.tsx
@@ -15,12 +15,13 @@
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 
-import {Button, Select} from 'native-base';
-import {useCallback, useMemo, useState} from 'react';
+import {Button} from 'native-base';
+import {useCallback, useMemo, useRef, useState} from 'react';
 import {useTranslation} from 'react-i18next';
-import {SoilIdProjectSoilSettingsDepthIntervalPresetChoices} from 'terraso-client-shared/graphqlSchema/graphql';
 import {
+  DEPTH_PRESETS,
   LabelledDepthInterval,
+  ProjectDepthIntervalPreset,
   updateProjectDepthInterval,
   updateProjectSoilSettings,
 } from 'terraso-client-shared/soilId/soilIdSlice';
@@ -28,20 +29,21 @@ import {ConfirmModal} from 'terraso-mobile-client/components/ConfirmModal';
 import {useProjectRoleContext} from 'terraso-mobile-client/context/ProjectRoleContext';
 import {useDispatch} from 'terraso-mobile-client/store';
 import {DepthIntervalTable} from 'terraso-mobile-client/screens/ProjectInputScreen/DepthIntervalTable';
-import {AddIntervalModal} from 'terraso-mobile-client/components/AddIntervalModal';
-import {DepthPresets} from 'terraso-mobile-client/constants';
-import {Modal} from 'terraso-mobile-client/components/Modal';
+import {AddIntervalModalBody} from 'terraso-mobile-client/components/AddIntervalModal';
+import {Modal, ModalHandle} from 'terraso-mobile-client/components/Modal';
 import {Icon} from 'terraso-mobile-client/components/Icons';
-import {Box, Text} from 'terraso-mobile-client/components/NativeBaseAdapters';
+import {
+  Box,
+  Heading,
+} from 'terraso-mobile-client/components/NativeBaseAdapters';
 import {useProjectSoilSettings} from 'terraso-client-shared/selectors';
+import {Select} from 'terraso-mobile-client/components/inputs/Select';
 
 export const SoilPitSettings = ({projectId}: {projectId: string}) => {
   const {t} = useTranslation();
+  const modalRef = useRef<ModalHandle>(null);
   const settings = useProjectSoilSettings(projectId);
-  const [selectedPreset, setSelectedPreset] =
-    useState<SoilIdProjectSoilSettingsDepthIntervalPresetChoices>(
-      settings.depthIntervalPreset,
-    );
+  const [newPreset, setNewPreset] = useState(settings.depthIntervalPreset);
   const dispatch = useDispatch();
 
   const projectRole = useProjectRoleContext();
@@ -64,49 +66,43 @@ export const SoilPitSettings = ({projectId}: {projectId: string}) => {
     dispatch(
       updateProjectSoilSettings({
         projectId,
-        depthIntervalPreset: selectedPreset,
+        depthIntervalPreset: newPreset,
       }),
     );
-  }, [dispatch, projectId, selectedPreset]);
+  }, [dispatch, projectId, newPreset]);
 
   const onSelectUpdated = useCallback(
-    (onOpen: () => void) => (value: string) => {
-      if (value === selectedPreset) {
+    (value: ProjectDepthIntervalPreset) => {
+      if (value === settings.depthIntervalPreset) {
         return;
       }
-      setSelectedPreset(
-        value as SoilIdProjectSoilSettingsDepthIntervalPresetChoices,
-      );
-      onOpen();
+      setNewPreset(value);
+      modalRef.current?.onOpen();
     },
+    [setNewPreset, settings.depthIntervalPreset],
+  );
 
-    [selectedPreset, setSelectedPreset],
+  const renderPreset = useCallback(
+    (preset: ProjectDepthIntervalPreset) =>
+      t(`projects.inputs.depth_intervals.preset.${preset}`),
+    [t],
   );
 
   return (
     <Box p={4}>
-      <Text color={'primary.main'}>
-        {t('projects.inputs.depth_intervals.title')}
-      </Text>
       <ConfirmModal
-        trigger={onOpen => (
+        ref={modalRef}
+        trigger={_ => (
           <Select
-            mb={5}
-            width={'60%'}
-            variant={'underlined'}
-            selectedValue={settings.depthIntervalPreset}
-            onValueChange={onSelectUpdated(onOpen)}
-            isDisabled={!userCanUpdateIntervals}>
-            {DepthPresets.map(preset => (
-              <Select.Item
-                label={t(
-                  `projects.inputs.depth_intervals.${preset.toLowerCase()}`,
-                )}
-                value={preset}
-                key={preset}
-              />
-            ))}
-          </Select>
+            nullable={false}
+            options={DEPTH_PRESETS}
+            value={settings.depthIntervalPreset}
+            onValueChange={onSelectUpdated}
+            renderValue={renderPreset}
+            label={t('projects.inputs.depth_intervals.title')}
+            disabled={!userCanUpdateIntervals}
+            maxWidth={250}
+          />
         )}
         title={t('projects.inputs.depth_intervals.confirm_preset.title')}
         body={t('projects.inputs.depth_intervals.confirm_preset.body')}
@@ -133,8 +129,11 @@ export const SoilPitSettings = ({projectId}: {projectId: string}) => {
               leftIcon={<Icon name="add" />}>
               {t('soil.add_depth_label')}
             </Button>
-          )}>
-          <AddIntervalModal
+          )}
+          Header={
+            <Heading variant="h6">{t('soil.depth_interval.add_title')}</Heading>
+          }>
+          <AddIntervalModalBody
             onSubmit={onAddDepthInterval}
             existingIntervals={settings.depthIntervals}
           />
