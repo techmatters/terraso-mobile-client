f75b7be910fdce2733ad7eb6698564b9e6760381
Carissa Knipe <carissa@techmatters.org>
2024-04-10 20:18:38 -0700

    refactor: Remove unnecessary export and JSX, and add copyright header

--- dev-client/src/screens/SlopeScreen/components/SlopeInfoContent.tsx
+++ dev-client/src/screens/SlopeScreen/components/SlopeInfoContent.tsx
@@ -1,5 +1,21 @@
+/*
+ * Copyright © 2024 Technology Matters
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
 import {BottomSheetScrollView} from '@gorhom/bottom-sheet';
-import {Trans, useTranslation} from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 
 import {
   Column,
@@ -16,24 +32,9 @@ export const SlopeInfoContent = () => {
         <Heading w="full" textAlign="left">
           {t('slope.info.title')}
         </Heading>
-        <Text variant="body1">
-          <Trans i18nKey="slope.info.description1">
-            <Text bold>first</Text>
-            <Text>second</Text>
-          </Trans>
-        </Text>
-        <Text variant="body1">
-          <Trans i18nKey="slope.info.description2">
-            <Text bold>first</Text>
-            <Text>second</Text>
-          </Trans>
-        </Text>
-        <Text variant="body1">
-          <Trans i18nKey="slope.info.description3">
-            <Text bold>first</Text>
-            <Text>second</Text>
-          </Trans>
-        </Text>
+        <Text variant="body1">{t('slope.info.description1')}</Text>
+        <Text variant="body1">{t('slope.info.description2')}</Text>
+        <Text variant="body1">{t('slope.info.description3')}</Text>
       </Column>
     </BottomSheetScrollView>
   );
