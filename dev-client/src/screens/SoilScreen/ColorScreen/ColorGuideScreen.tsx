b933294b4dc92a097a65df95e21f312d70e58ae9
Carissa Knipe <carissa@techmatters.org>
2024-04-18 19:00:16 -0700

    fix: update Soil Color Guide to only display metric or imperial units, not both

--- dev-client/src/screens/SoilScreen/ColorScreen/ColorGuideScreen.tsx
+++ dev-client/src/screens/SoilScreen/ColorScreen/ColorGuideScreen.tsx
@@ -17,7 +17,7 @@
 
 import {Button} from 'native-base';
 import {useCallback} from 'react';
-import {useTranslation} from 'react-i18next';
+import {Trans, useTranslation} from 'react-i18next';
 import {Image, ScrollView, StyleSheet} from 'react-native';
 import {BulletList} from 'terraso-mobile-client/components/BulletList';
 import {Icon} from 'terraso-mobile-client/components/Icons';
@@ -50,6 +50,9 @@ export const ColorGuideScreen = (props: SoilPitInputScreenProps) => {
 
   const stepContent = [
     <>
+      <Paragraph variant="body1">
+        {t('soil.color.guide.step1.content')}
+      </Paragraph>
       <BulletList
         data={[1, 2, 3, 4, 5]}
         renderItem={i => (
@@ -62,48 +65,72 @@ export const ColorGuideScreen = (props: SoilPitInputScreenProps) => {
         />
       </Box>
     </>,
-    <Row>
-      <Box flex={1}>
-        <Box width="100%" aspectRatio={1}>
-          <Image
-            resizeMode="contain"
-            style={styles.image}
-            source={require('terraso-mobile-client/assets/color/sieve.png')}
-          />
+
+    <>
+      <Paragraph variant="body1">
+        <Trans
+          i18nKey="soil.color.guide.step2.content"
+          values={{units: 'METRIC'}}
+        />
+      </Paragraph>
+      <Row>
+        <Box flex={1}>
+          <Box width="100%" aspectRatio={1}>
+            <Image
+              resizeMode="contain"
+              style={styles.image}
+              source={require('terraso-mobile-client/assets/color/sieve.png')}
+            />
+          </Box>
         </Box>
-      </Box>
-      <Box width="md" />
-      <Box flex={1}>
-        <Box width="100%" aspectRatio={1}>
-          <Image
-            resizeMode="contain"
-            style={styles.image}
-            source={require('terraso-mobile-client/assets/color/flat-pile.png')}
-          />
+        <Box width="md" />
+        <Box flex={1}>
+          <Box width="100%" aspectRatio={1}>
+            <Image
+              resizeMode="contain"
+              style={styles.image}
+              source={require('terraso-mobile-client/assets/color/flat-pile.png')}
+            />
+          </Box>
         </Box>
+      </Row>
+    </>,
+
+    <Paragraph variant="body1">
+      {t('soil.color.guide.step3.content')}
+    </Paragraph>,
+
+    <>
+      <Paragraph variant="body1">
+        {t('soil.color.guide.step4.content')}
+      </Paragraph>
+      <Box width="100%">
+        <Image
+          source={require('terraso-mobile-client/assets/color/reference.jpg')}
+        />
       </Box>
-    </Row>,
-    undefined,
-    <Box width="100%">
-      <Image
-        source={require('terraso-mobile-client/assets/color/reference.jpg')}
-      />
-    </Box>,
-    <Row justifyContent="space-between">
-      <Button variant="link" onPress={onGoBack}>
-        {t('soil.color.guide.go_back')}
-      </Button>
-      <ImagePicker onPick={onTakePhoto}>
-        {onOpen => (
-          <Button
-            _text={{textTransform: 'uppercase'}}
-            leftIcon={<Icon name="camera" />}
-            onPress={onOpen}>
-            {t('soil.color.guide.take_photo')}
-          </Button>
-        )}
-      </ImagePicker>
-    </Row>,
+    </>,
+
+    <>
+      <Paragraph variant="body1">
+        {t('soil.color.guide.step5.content')}
+      </Paragraph>
+      <Row justifyContent="space-between">
+        <Button variant="link" onPress={onGoBack}>
+          {t('soil.color.guide.go_back')}
+        </Button>
+        <ImagePicker onPick={onTakePhoto}>
+          {onOpen => (
+            <Button
+              _text={{textTransform: 'uppercase'}}
+              leftIcon={<Icon name="camera" />}
+              onPress={onOpen}>
+              {t('soil.color.guide.take_photo')}
+            </Button>
+          )}
+        </ImagePicker>
+      </Row>
+    </>,
   ] satisfies React.ReactNode[];
 
   return (
@@ -115,9 +142,6 @@ export const ColorGuideScreen = (props: SoilPitInputScreenProps) => {
               <Text variant="body1-strong">
                 {t(`soil.color.guide.step${index + 1}.title`)}
               </Text>
-              <Paragraph variant="body1">
-                {t(`soil.color.guide.step${index + 1}.content`)}
-              </Paragraph>
               {content}
             </Column>
           ))}
