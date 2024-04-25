77576edaf0cd5d040e6918e9841bd09f6768a091
Carissa Knipe <carissa@techmatters.org>
2024-04-25 16:52:01 -0700

    chore: Remove unnecessary comment

--- dev-client/src/components/icons/LinkNewWindowIcon.tsx
+++ dev-client/src/components/icons/LinkNewWindowIcon.tsx
@@ -73,14 +73,3 @@ const styles = StyleSheet.create({
     verticalAlign: 'middle',
   },
 });
-// import {View} from 'react-native';
-// import {Icon} from 'terraso-mobile-client/components/icons/Icon';
-
-// export const LinkNewWindowIcon = () => {
-//   return (
-//     // eslint-disable-next-line react-native/no-inline-styles
-//     <View style={{flexDirection: 'row'}}>
-//       <Icon name="open-in-new" color="primary.main" size={14} />
-//     </View>
-//   );
-// };
