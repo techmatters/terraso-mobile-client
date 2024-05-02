0fbde356d1ffae9a7cb3990051659afbeb9c8fa7
Ruxandra Machedon <ruxandra@techmatters.org>
2024-05-02 19:43:02 -0400

    feat: integrate logout modal with settings button

--- dev-client/src/screens/SettingsScreen/components/actions/LogOutButton.tsx
+++ dev-client/src/screens/SettingsScreen/components/actions/LogOutButton.tsx
@@ -1,19 +1,25 @@
 import {Button} from 'native-base';
 import {useTranslation} from 'react-i18next';
 import {Icon} from 'terraso-mobile-client/components/Icons';
+import {LogoutModal} from 'terraso-mobile-client/components/modals/LogoutModal';
 
 export function LogOutButton() {
   const {t} = useTranslation();
 
   return (
-    <Button
-      size="md"
-      variant="ghost"
-      alignSelf="flex-start"
-      _text={{color: 'text.primary'}}
-      textTransform={'uppercase'}
-      leftIcon={<Icon name="logout" color="text.primary" />}>
-      {t('settings.logOut')}
-    </Button>
+    <LogoutModal
+      trigger={onOpen => (
+        <Button
+          size="md"
+          variant="ghost"
+          alignSelf="flex-start"
+          _text={{color: 'text.primary'}}
+          textTransform={'uppercase'}
+          leftIcon={<Icon name="logout" color="text.primary" />}
+          onPress={onOpen}>
+          {t('settings.logOut')}
+        </Button>
+      )}
+    />
   );
 }
