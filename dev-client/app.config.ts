5d351547b57e34cf215694dd42ff6064d8215113
Paul Schreiber <paulschreiber@gmail.com>
2024-05-28 10:12:42 -0400

    chore: set build number to 3

--- dev-client/app.config.ts
+++ dev-client/app.config.ts
@@ -104,7 +104,7 @@ export default ({config}: ConfigContext): ExpoConfig => ({
   },
   ios: {
     bundleIdentifier: 'org.terraso.test.Terraso-LandPKS',
-    buildNumber: appVersion,
+    buildNumber: 3,
     icon: 'src/assets/landpks-round.png',
     supportsTablet: true,
     requireFullScreen: true,
