62458e399a454a661ea990379e8c174eed49d142
Paul Schreiber <paul@techmatters.org>
2024-05-31 12:53:02 -0400

    fix: use label for site name text field

--- dev-client/src/screens/CreateSiteScreen/components/CreateSiteForm.tsx
+++ dev-client/src/screens/CreateSiteScreen/components/CreateSiteForm.tsx
@@ -74,7 +74,10 @@ export const CreateSiteForm = ({
       <ScrollView>
         <VStack p="16px" pt="30px" space="18px">
           <FormField name="name">
-            <FormInput placeholder={t('site.create.name_placeholder')} />
+            <FormInput
+              placeholder={t('site.create.name_placeholder')}
+              textInputLabel={t('site.create.name_placeholder')}
+            />
           </FormField>
 
           <FormLabel>{t('site.create.location_label')}</FormLabel>
