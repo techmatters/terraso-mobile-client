e1e82193d15451cb9b96150fc182765da21970a3
Carissa Knipe <carissa@techmatters.org>
2024-06-10 13:48:30 -0700

    fix: slightly widen box for percentage in SoilMatchTile (#1538)

--- dev-client/src/screens/LocationScreens/components/soilId/SoilMatchTile.tsx
+++ dev-client/src/screens/LocationScreens/components/soilId/SoilMatchTile.tsx
@@ -37,8 +37,9 @@ export const SoilMatchTile = ({soil_name, score, onPress}: Props) => {
         justifyContent="space-between"
         my="4px"
         py="4px">
-        <Box marginHorizontal="16px" width="74px" justifyContent="center">
+        <Box marginHorizontal="16px" width="78px" justifyContent="center">
           <Text
+            variant="score-tile"
             size="30px"
             fontWeight={400}
             color="primary.lighter"
