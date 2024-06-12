9cbd70740717c9ff8c39717bfc3c3d234b303bee
Carissa Knipe <carissa@techmatters.org>
2024-06-12 16:17:17 -0700

    feat: 'Add Team Member' screen opens with new design and does all the validation at the same time, whenever the field entry changes

--- dev-client/src/screens/AddUserToProjectScreen/AddUserToProjectScreen.tsx
+++ dev-client/src/screens/AddUserToProjectScreen/AddUserToProjectScreen.tsx
@@ -15,31 +15,21 @@
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 
-import {useCallback, useMemo, useState} from 'react';
 import {useTranslation} from 'react-i18next';
 
+import {Formik} from 'formik';
 import {Button} from 'native-base';
+import * as yup from 'yup';
 
 import {checkUserInProject} from 'terraso-client-shared/account/accountService';
-import {
-  addUserToProject,
-  ProjectRole,
-} from 'terraso-client-shared/project/projectSlice';
 
-import {
-  Box,
-  Row,
-  Text,
-} from 'terraso-mobile-client/components/NativeBaseAdapters';
+import {ScreenContentSection} from 'terraso-mobile-client/components/content/ScreenContentSection';
+import {FormInput} from 'terraso-mobile-client/components/form/FormInput';
+import {Icon} from 'terraso-mobile-client/components/icons/Icon';
+import {Box, Text} from 'terraso-mobile-client/components/NativeBaseAdapters';
 import {AppBar} from 'terraso-mobile-client/navigation/components/AppBar';
-import {useNavigation} from 'terraso-mobile-client/navigation/hooks/useNavigation';
-import {FreeformTextInput} from 'terraso-mobile-client/screens/AddUserToProjectScreen/components/FreeformTextInput';
-import MembershipControlList, {
-  UserWithRole,
-} from 'terraso-mobile-client/screens/AddUserToProjectScreen/components/MembershipControlList';
-import {useKeyboardOpen} from 'terraso-mobile-client/screens/AddUserToProjectScreen/hooks/useKeyboardOpen';
 import {ScreenScaffold} from 'terraso-mobile-client/screens/ScreenScaffold';
-import {useDispatch, useSelector} from 'terraso-mobile-client/store';
+import {useSelector} from 'terraso-mobile-client/store';
 
 type Props = {
   projectId: string;
@@ -47,123 +37,189 @@ type Props = {
 
 export const AddUserToProjectScreen = ({projectId}: Props) => {
   const {t} = useTranslation();
-  const [userRecord, setUserRecord] = useState<Record<string, UserWithRole>>(
-    {},
-  );
-  const keyboardOpen = useKeyboardOpen();
+  // TODO-cknipe: Remove this or put it in the appropriate place
+  // const [userRecord, setUserRecord] = useState<Record<string, UserWithRole>>(
+  //   {},
+  // );
+  // const keyboardOpen = useKeyboardOpen();
 
-  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
-  const navigation = useNavigation();
-  const dispatch = useDispatch();
+  // const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  // const navigation = useNavigation();
+  // const dispatch = useDispatch();
 
   const projectName = useSelector(
     state => state.project.projects[projectId]?.name,
   );
 
-  const userList = useMemo(() => Object.values(userRecord), [userRecord]);
-  const disableSubmit = useMemo(
-    () => isSubmitting || Object.keys(userRecord).length === 0,
-    [userRecord, isSubmitting],
-  );
+  // TODO-cknipe: Remove this or put it in the appropriate place
+  // const userList = useMemo(() => Object.values(userRecord), [userRecord]);
+  // const disableSubmit = useMemo(
+  //   () => isSubmitting || Object.keys(userRecord).length === 0,
+  //   [userRecord, isSubmitting],
+  // );
 
-  const validationFunc = async (email: string) => {
+  // // TODO-cknipe: Remove this
+  // const validationFunc = async (email: string) => {
+  //   if (email === '') {
+  //     // TODO: current bug means empty string for email is considered existing :(
+  //     // Easier just to explicitly reject it here
+  //     return t('projects.add_user.empty_email');
+  //   }
+  //   const userExists = await checkUserInProject(projectId, email);
+  //   if ('type' in userExists) {
+  //     switch (userExists.type) {
+  //       case 'NoUser':
+  //         return t('projects.add_user.user_does_not_exist', {email: email});
+  //       case 'InProject':
+  //         return t('projects.add_user.user_in_project', {email: email});
+  //     }
+  //   }
+  //   if (userExists.id in userRecord) {
+  //     return t('projects.add_user.already_added', {email: email});
+  //   }
+
+  //   setUserRecord(users => {
+  //     return {
+  //       ...users,
+  //       [userExists.id]: {user: userExists, role: 'VIEWER'},
+  //     };
+  //   });
+  //   return null;
+  // };
+
+  // const updateUserRole = useCallback((role: ProjectRole, userId: string) => {
+  //   setUserRecord(users => {
+  //     const newUsers = {...users};
+  //     newUsers[userId].role = role;
+  //     return newUsers;
+  //   });
+  // }, []);
+
+  // const removeUser = useCallback((userId: string) => {
+  //   setUserRecord(users => {
+  //     let newUsers = {...users};
+  //     delete newUsers[userId];
+  //     return newUsers;
+  //   });
+  // }, []);
+
+  // const submitUsers = async () => {
+  //   setIsSubmitting(true);
+  //   for (const {
+  //     user: {id: userId},
+  //     role,
+  //   } of Object.values(userRecord)) {
+  //     try {
+  //       dispatch(addUserToProject({userId, role, projectId}));
+  //     } catch (e) {
+  //       console.error(e);
+  //     }
+  //   }
+  //   navigation.pop();
+  //   setIsSubmitting(false);
+  // };
+
+  return (
+    <ScreenScaffold AppBar={<AppBar title={projectName} />}>
+      <ScreenContentSection title={t('projects.add_user.heading')}>
+        <Text variant="body1">{t('projects.add_user.help_text')}</Text>
+        <Box mt="md">
+          <AddTeamMemberForm projectId={projectId} />
+        </Box>
+      </ScreenContentSection>
+    </ScreenScaffold>
+  );
+};
+
+// TODO-cknipe: Move this to new file?
+type FormValues = {
+  email: string;
+};
+
+type FormProps = {
+  projectId: string;
+};
+
+const AddTeamMemberForm = ({projectId}: FormProps) => {
+  const {t} = useTranslation();
+
+  const onNext = async (values: FormValues) => {
+    // TODO-cknipe: Do the right thing on Next button
+    const validationErrors = await backendValidate(values.email);
+    console.log('Validation errors: ', validationErrors);
+  };
+
+  const backendValidate = async (email: string) => {
     if (email === '') {
-      // TODO: current bug means empty string for email is considered existing :(
-      // Easier just to explicitly reject it here
-      return t('projects.add_user.empty_email');
+      return t('general.required');
     }
-    const userExists = await checkUserInProject(projectId, email);
-    if ('type' in userExists) {
-      switch (userExists.type) {
+
+    const userOrError = await checkUserInProject(projectId, email);
+    if ('type' in userOrError) {
+      switch (userOrError.type) {
         case 'NoUser':
           return t('projects.add_user.user_does_not_exist', {email: email});
         case 'InProject':
           return t('projects.add_user.user_in_project', {email: email});
       }
     }
-    if (userExists.id in userRecord) {
+
+    // TODO-cknipe: What's userRecord? Remove this or use the real one.
+    const userRecord = ['TODO-cknipe'];
+    if (userOrError.id in userRecord) {
       return t('projects.add_user.already_added', {email: email});
     }
 
-    setUserRecord(users => {
-      return {
-        ...users,
-        [userExists.id]: {user: userExists, role: 'VIEWER'},
-      };
-    });
-    return null;
+    return true;
   };
 
-  const updateUserRole = useCallback((role: ProjectRole, userId: string) => {
-    setUserRecord(users => {
-      const newUsers = {...users};
-      newUsers[userId].role = role;
-      return newUsers;
-    });
-  }, []);
-
-  const removeUser = useCallback((userId: string) => {
-    setUserRecord(users => {
-      let newUsers = {...users};
-      delete newUsers[userId];
-      return newUsers;
-    });
-  }, []);
-
-  const submitUsers = async () => {
-    setIsSubmitting(true);
-    for (const {
-      user: {id: userId},
-      role,
-    } of Object.values(userRecord)) {
-      try {
-        dispatch(addUserToProject({userId, role, projectId}));
-      } catch (e) {
-        console.error(e);
-      }
-    }
-    navigation.pop();
-    setIsSubmitting(false);
-  };
+  // TODO-cknipe: Don't do the backend call every time
+  // Debounce or only do the backend validation when "Next" is pressed
+  const validationSchema = yup.object().shape({
+    email: yup
+      .string()
+      .required(t('projects.add_user.empty_email'))
+      .email(t('projects.add_user.invalid_email'))
+      .test(async (value, {createError}) => {
+        const validationResult = await backendValidate(value);
+        return validationResult === true
+          ? true
+          : createError({
+              message: validationResult,
+            });
+      }),
+  });
 
   return (
-    <ScreenScaffold AppBar={<AppBar title={projectName} />}>
-      <Box mx="5%" mb="15px" mt="22px">
-        <Text variant="body1" fontWeight="bold">
-          {t('projects.add_user.heading')}
-        </Text>
-        <Text variant="body1">{t('projects.add_user.help_text')}</Text>
-      </Box>
-      <Box mx="5%" mb="15px">
-        <FreeformTextInput
-          validationFunc={validationFunc}
-          placeholder={t('general.email_placeholder')}
-          inputProps={{
-            autoComplete: 'email',
-            autoCapitalize: 'none',
-            keyboardType: 'email-address',
-            label: t('general.email_label'),
-          }}
-        />
-      </Box>
-      <MembershipControlList
-        users={userList}
-        updateUserRole={updateUserRole}
-        removeUser={removeUser}
-      />
-      <Row
-        flexDirection="row-reverse"
-        my="20px"
-        ml="20px"
-        display={keyboardOpen ? 'none' : undefined}>
-        <Button
-          onPress={submitUsers}
-          isDisabled={disableSubmit}
-          isLoading={isSubmitting}
-          w="100px">
-          {t('general.save_fab')}
-        </Button>
-      </Row>
-    </ScreenScaffold>
+    <Formik
+      initialValues={{email: ''}}
+      validationSchema={validationSchema}
+      validateOnMount={true}
+      initialTouched={{
+        email: true,
+      }}
+      onSubmit={onNext}>
+      {({handleSubmit, isValid, isValidating, isSubmitting}) => {
+        return (
+          <>
+            <FormInput
+              key="email"
+              name="email"
+              textInputLabel={t('general.email_label')}
+              placeholder={t('general.email_placeholder')}
+            />
+            <Button
+              mt="sm"
+              alignSelf="flex-end"
+              rightIcon={<Icon name="chevron-right" />}
+              onPress={handleSubmit}
+              disabled={!isValid || isValidating || isSubmitting}
+              _text={{textTransform: 'uppercase'}}>
+              {t('general.next')}
+            </Button>
+          </>
+        );
+      }}
+    </Formik>
   );
 };
