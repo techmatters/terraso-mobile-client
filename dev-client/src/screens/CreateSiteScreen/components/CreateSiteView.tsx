249e321deaada8c6954dc6fd4546fb08842a213a
Paul Schreiber <paul@techmatters.org>
2024-06-12 23:07:04 -0400

    fix: after creating site, navigate to home screen
    before this change, if you went to the Learn More screen, pop would take you back to the temporary site dashboard
    
    Similar to https://github.com/techmatters/terraso-mobile-client/pull/1431

--- dev-client/src/screens/CreateSiteScreen/components/CreateSiteView.tsx
+++ dev-client/src/screens/CreateSiteScreen/components/CreateSiteView.tsx
@@ -68,7 +68,7 @@ export const CreateSiteView = ({
       const createdSite = await createSiteCallback({...site, elevation});
       if (createdSite !== undefined) {
         homeScreen?.showSiteOnMap(createdSite);
-        navigation.pop();
+        navigation.navigate('BOTTOM_TABS');
       }
     },
     [createSiteCallback, navigation, validationSchema, homeScreen, elevation],
