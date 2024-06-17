d8650f880be1955b3ffd05e19f91b03d976b535d
Ruxandra Machedon <ruxandra@techmatters.org>
2024-06-17 10:52:28 -0400

    fix: garbled github suggestion

--- dev-client/src/screens/DeleteAccountScreen/components/DeleteAccountConfirmForm.tsx
+++ dev-client/src/screens/DeleteAccountScreen/components/DeleteAccountConfirmForm.tsx
@@ -52,7 +52,6 @@ export function DeleteAccountConfirmForm({
         autoCapitalize="none"
         keyboardType="email-address"
       />
-      ```
       <Row space="8px" marginHorizontal="24px" alignSelf="flex-end">
         <Button onPress={goBack} {...ACTION_BUTTON_VARIANTS.cancel}>
           {t('delete_account.confirm.cancel')}
