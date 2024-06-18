techmatters/terraso-mobile-client

commit 1534a9af905cca1249fdfea3ccb0a915327a2468

fix: pass elevation data through to Learn More screen (#1554)
* fix: pass elevation data through to Learn More screen

* fix: after creating site, navigate to home screen

before this change, if you went to the Learn More screen, pop would take you back to the temporary site dashboard

Similar to https://github.com/techmatters/terraso-mobile-client/pull/1431

dev-client/src/screens/HomeScreen/components/TemporaryLocationCallout.tsx
@@ -66,8 +66,11 @@ export const TemporaryLocationCallout = ({
   }, [closeCallout, navigation, coords, elevation]);
 
   const onLearnMore = useCallback(() => {
-    navigation.navigate('LOCATION_DASHBOARD', {coords});
-  }, [navigation, coords]);
+    navigation.navigate('LOCATION_DASHBOARD', {
+      coords,
+      elevation: siteElevationValue,
+    });
+  }, [navigation, coords, siteElevationValue]);
 
   return (
     <Card

dev-client/src/screens/CreateSiteScreen/components/CreateSiteView.tsx
@@ -65,7 +65,7 @@ export const CreateSiteView = ({
       const createdSite = await createSiteCallback({...site, elevation});
       if (createdSite !== undefined) {
         homeScreen?.showSiteOnMap(createdSite);
-        navigation.pop();
+        navigation.navigate('BOTTOM_TABS');
       }
     },
     [createSiteCallback, navigation, validationSchema, homeScreen, elevation],

dev-client/src/screens/LocationScreens/LocationDashboardScreen.tsx
@@ -39,7 +39,9 @@ import {ScreenScaffold} from 'terraso-mobile-client/screens/ScreenScaffold';
 import {useSelector} from 'terraso-mobile-client/store';
 import {isSiteManager} from 'terraso-mobile-client/util';
 
-type Props = ({siteId: string} | {coords: Coords}) & {initialTab?: SiteTabName};
+type Props = ({siteId: string} | {coords: Coords} | {elevation: number}) & {
+  initialTab?: SiteTabName;
+};
 
 // A "Location" can refer to a "Site" (with siteId) xor a "Temporary Location" (with only coords)
 export const LocationDashboardScreen = (props: Props) => {
@@ -53,6 +55,8 @@ export const LocationDashboardScreen = (props: Props) => {
     siteId === undefined ? undefined : selectSite(siteId)(state),
   );
   const coords = 'coords' in props ? props.coords : site!;
+
+  const elevation = 'elevation' in props ? props.elevation : undefined;
 
   const userRole = useSelector(state =>
     siteId === undefined ? null : selectUserRoleSite(state, siteId),
@@ -103,7 +107,11 @@ export const LocationDashboardScreen = (props: Props) => {
             />
           </SiteRoleContextProvider>
         ) : (
-          <LocationDashboardContent siteId={siteId} coords={coords} />
+          <LocationDashboardContent
+            siteId={siteId}
+            coords={coords}
+            elevation={elevation}
+          />
         )}
         <PrivacyInfoModal ref={infoModalRef} onClose={onInfoClose} />
       </ScreenScaffold>

commit 006cfc4ca1ebd8a4767e27467ec8d94d88bc2fc0

fix: fix linter errors introduced after PR #1554

dev-client/src/screens/HomeScreen/components/TemporaryLocationCallout.tsx
@@ -68,9 +68,9 @@ export const TemporaryLocationCallout = ({
   const onLearnMore = useCallback(() => {
     navigation.navigate('LOCATION_DASHBOARD', {
       coords,
-      elevation: siteElevationValue,
+      elevation: elevation,
     });
-  }, [navigation, coords, siteElevationValue]);
+  }, [navigation, coords, elevation]);
 
   return (
     <Card