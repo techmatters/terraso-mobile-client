547cdc7302965b66357727e22f53c55d6710623b
Ruxandra Machedon <ruxandra@techmatters.org>
2024-06-24 15:57:01 -0400

    feat: add new privacy sheet based on common overlay sheet implementation

--- dev-client/src/components/sheets/privacy/DataPrivacyInfoSheetButton.tsx
+++ dev-client/src/components/sheets/privacy/DataPrivacyInfoSheetButton.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2023 Technology Matters
+ * Copyright © 2024 Technology Matters
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
@@ -15,6 +15,11 @@
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 
-import {createContext} from 'react';
+import {InfoButton} from 'terraso-mobile-client/components/buttons/InfoButton';
+import {DataPrivacyInfoSheet} from 'terraso-mobile-client/components/sheets/privacy/DataPrivacyInfoSheet';
 
-export const BottomSheetPrivacyModalContext = createContext(() => {});
+export const DataPrivacyInfoSheetButton = () => {
+  return (
+    <DataPrivacyInfoSheet trigger={onOpen => <InfoButton onPress={onOpen} />} />
+  );
+};
