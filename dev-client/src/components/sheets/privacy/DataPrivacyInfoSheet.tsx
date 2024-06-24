0c8b7a4c60f8ecabc2859b7630e7d1810b3fc762
Ruxandra Machedon <ruxandra@techmatters.org>
2024-06-24 15:57:01 -0400

    feat: add new privacy sheet based on common overlay sheet implementation

---
+++ dev-client/src/components/sheets/privacy/DataPrivacyInfoSheet.tsx
@@ -0,0 +1,40 @@
+/*
+ * Copyright © 2024 Technology Matters
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+import {useTranslation} from 'react-i18next';
+
+import {ModalTrigger} from 'terraso-mobile-client/components/modals/Modal';
+import {PrivacyInfoContent} from 'terraso-mobile-client/components/modals/privacy/PrivacyInfoContent';
+import {InfoOverlaySheet} from 'terraso-mobile-client/components/sheets/InfoOverlaySheet';
+
+type Props = {
+  trigger?: ModalTrigger;
+};
+
+export const DataPrivacyInfoSheet = ({
+  trigger,
+}: React.PropsWithChildren<Props>) => {
+  const {t} = useTranslation();
+
+  return (
+    <InfoOverlaySheet
+      trigger={trigger}
+      Header={t('general.info.privacy_title')}>
+      <PrivacyInfoContent />
+    </InfoOverlaySheet>
+  );
+};
