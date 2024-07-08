30061f4c66e9488c8ef8f0c4054f3488e0df856b
Paul Schreiber <paul@techmatters.org>
2024-07-08 14:25:43 -0400

    fix: add spacing between empty accordion sections

--- dev-client/src/screens/SiteTransferProjectScreen/SiteTransferProjectScreen.tsx
+++ dev-client/src/screens/SiteTransferProjectScreen/SiteTransferProjectScreen.tsx
@@ -208,6 +208,9 @@ export const SiteTransferProjectScreen = ({projectId}: Props) => {
                 {projectName} ({projectSites.length})
               </Text>
             }
+            boxProps={{
+              mb: projectSites.length === 0 ? '2px' : undefined,
+            }}
             initiallyOpen={projectSites.length > 0}
             disableOpen={projectSites.length === 0}>
             {projectSites.length > 0 ? (
