5bcee130e5f1826b2011caaa38c7d1d36380fae4
Paul Schreiber <paul@techmatters.org>
2024-08-06 11:07:44 -0400

    fix: hide photo conditions radio buttons from Viewers

--- dev-client/src/screens/SoilScreen/ColorScreen/components/PhotoConditions.tsx
+++ dev-client/src/screens/SoilScreen/ColorScreen/components/PhotoConditions.tsx
@@ -24,6 +24,8 @@ import {fromEntries} from 'terraso-client-shared/utils';
 
 import {Column} from 'terraso-mobile-client/components/NativeBaseAdapters';
 import {RadioBlock} from 'terraso-mobile-client/components/RadioBlock';
+import {RestrictBySiteRole} from 'terraso-mobile-client/components/RestrictByRole';
+import {SiteRoleContextProvider} from 'terraso-mobile-client/context/SiteRoleContext';
 import {SoilPitInputScreenProps} from 'terraso-mobile-client/screens/SoilScreen/components/SoilPitInputScreenScaffold';
 import {useDispatch, useSelector} from 'terraso-mobile-client/store';
 
@@ -96,17 +98,26 @@ export const PhotoConditions = (props: SoilPitInputScreenProps) => {
   );
 
   return (
-    <Column paddingHorizontal="md">
-      <RadioBlock
-        label={t('soil.color.soil_condition')}
-        options={soilOptions}
-        groupProps={soilGroupProps}
-      />
-      <RadioBlock
-        label={t('soil.color.lighting_condition')}
-        options={lightingOptions}
-        groupProps={lightingGroupProps}
-      />
-    </Column>
+    <SiteRoleContextProvider siteId={props.siteId}>
+      <RestrictBySiteRole
+        role={[
+          {kind: 'project', role: 'MANAGER'},
+          {kind: 'project', role: 'CONTRIBUTOR'},
+          {kind: 'site', role: 'OWNER'},
+        ]}>
+        <Column paddingHorizontal="md">
+          <RadioBlock
+            label={t('soil.color.soil_condition')}
+            options={soilOptions}
+            groupProps={soilGroupProps}
+          />
+          <RadioBlock
+            label={t('soil.color.lighting_condition')}
+            options={lightingOptions}
+            groupProps={lightingGroupProps}
+          />
+        </Column>
+      </RestrictBySiteRole>
+    </SiteRoleContextProvider>
   );
 };
