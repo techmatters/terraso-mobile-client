e12b1d7c873c39f95fb5c375de37def9b1f2f58a
Carissa Knipe <carissa@techmatters.org>
2024-09-10 17:47:02 -0700

    feat: Add logic from Whitespectre to stop monitoring when app is not active. However, it should not live in the custom hook, as it will be executed any time another component uses the hook. Instead, it should run only once in a top-level component

--- dev-client/src/hooks/connectivityHooks.ts
+++ dev-client/src/hooks/connectivityHooks.ts
@@ -15,32 +15,68 @@
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
+import {AppState} from 'react-native';
 
-import {addEventListener} from '@react-native-community/netinfo';
+import {
+  addEventListener,
+  NetInfoSubscription,
+} from '@react-native-community/netinfo';
 
 export const useIsOffline = () => {
   const [isOffline, setIsOffline] = useState<boolean | null>(false);
+  const netInfoSubscriptionRef = useRef<NetInfoSubscription | null>(null);
+  // Question: Do we need to use lostConnection as the example? They had it as a ref that would trigger some data refresh
   // TODO: Maybe make this a type that can be "Offline" | "Unknown" | "Connected"
 
   useEffect(() => {
-    const unsubscribe = addEventListener(state => {
-      // TODO: Remove this
-      console.log(
-        'Connected =',
-        state.isConnected,
-        ' | Reachable =',
-        state.isInternetReachable,
-      );
+    const stopMonitoringReachability = () => {
+      if (netInfoSubscriptionRef.current) {
+        netInfoSubscriptionRef.current?.();
+        netInfoSubscriptionRef.current = null;
+      }
+      // TODO: Should we do setIsOffline(undefined) when we stop monitoring, or just keep it what it last was?
+    };
 
-      // Return null if isConnected is null -- make a test for that cuz
-      //   false && null = false
-      //   null && false = null
-      const isOnline = state.isConnected && state.isInternetReachable;
-      setIsOffline(isOnline === null ? null : !isOnline);
+    const startMonitoringReachability = () => {
+      stopMonitoringReachability();
 
-      return () => unsubscribe();
+      netInfoSubscriptionRef.current = addEventListener(state => {
+        // TODO: Remove this
+        console.log(
+          'Connected =',
+          state.isConnected,
+          ' | Reachable =',
+          state.isInternetReachable,
+        );
+
+        // Return null if isConnected is null -- make a test for that cuz
+        //   false && null = false
+        //   null && false = null
+        const isOnline = state.isConnected && state.isInternetReachable;
+        setIsOffline(isOnline === null ? null : !isOnline);
+      });
+    };
+
+    if (AppState.currentState === 'active') {
+      startMonitoringReachability();
+    }
+
+    console.log('add app state listener');
+    const appStateListener = AppState.addEventListener('change', state => {
+      if (state === 'active') {
+        startMonitoringReachability();
+      } else {
+        stopMonitoringReachability();
+      }
     });
+
+    return () => {
+      // Won't this get called when any component that uses this hook gets unmounted? Isn't that more than we want?
+      // stopMonitoringReachability();
+      console.log('remove app state listener');
+      appStateListener.remove();
+    };
   }, []);
 
   return isOffline;
