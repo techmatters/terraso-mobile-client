de3ed64d5dcd87d7bd80e32f4e6a383366bc3b0b
Carissa Knipe <carissa@techmatters.org>
2024-10-15 18:03:37 -0700

    test: Add navigation to tests

--- dev-client/__tests__/integration/EditSiteNoteScreen.test.tsx
+++ dev-client/__tests__/integration/EditSiteNoteScreen.test.tsx
@@ -3,6 +3,21 @@ import {render} from '@testing/integration/utils';
 
 import {EditSiteNoteScreen} from 'terraso-mobile-client/screens/SiteNotesScreen/EditSiteNoteScreen';
 
+const mockedNavigate = jest.fn();
+jest.mock('terraso-mobile-client/navigation/hooks/useNavigation', () => {
+  const actualNav = jest.requireActual(
+    'terraso-mobile-client/navigation/hooks/useNavigation',
+  );
+  return {
+    ...actualNav,
+    useNavigation: () => ({navigate: mockedNavigate}),
+  };
+});
+
+afterEach(() => {
+  mockedNavigate.mockClear();
+});
+
 test('renders if data exists', () => {
   const screen = render(<EditSiteNoteScreen noteId="note1" siteId="1" />, {
     route: 'EDIT_SITE_NOTE',
@@ -11,6 +26,7 @@ test('renders if data exists', () => {
 
   expect(screen.getByText('Site Note')).toBeOnTheScreen();
   expect(screen.getByDisplayValue('note 1 contents')).toBeOnTheScreen();
+  expect(mockedNavigate).toHaveBeenCalledTimes(0);
 });
 
 test('renders null if site missing', () => {
@@ -22,8 +38,11 @@ test('renders null if site missing', () => {
     },
   );
 
+  // Ideally would want to test that navigation worked, but I can't figure out how to do that
   expect(screen.queryByText('Site Note')).toBeNull();
   expect(screen.queryByText('note 1 contents')).toBeNull();
+  expect(mockedNavigate).toHaveBeenCalledTimes(1);
+  expect(mockedNavigate).toHaveBeenCalledWith('BOTTOM_TABS');
 });
 
 test('renders null if note missing', () => {
@@ -37,4 +56,9 @@ test('renders null if note missing', () => {
 
   expect(screen.queryByText('Site Note')).toBeNull();
   expect(screen.queryByText('note 1 contents')).toBeNull();
+  expect(mockedNavigate).toHaveBeenCalledTimes(1);
+  expect(mockedNavigate).toHaveBeenCalledWith('SITE_TABS', {
+    initialTab: 'NOTES',
+    siteId: '1',
+  });
 });
