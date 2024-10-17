557ed77ed511b9bb2de5608a78b2bf695e98a5a4
Carissa Knipe <carissa@techmatters.org>
2024-10-17 15:37:12 -0700

    fix: Wrap tests in describe()

--- dev-client/__tests__/integration/EditSiteNoteScreen.test.tsx
+++ dev-client/__tests__/integration/EditSiteNoteScreen.test.tsx
@@ -35,47 +35,49 @@ afterEach(() => {
   mockedNavigate.mockClear();
 });
 
-test('renders if data exists', () => {
-  const screen = render(<EditSiteNoteScreen noteId="note1" siteId="1" />, {
-    route: 'EDIT_SITE_NOTE',
-    initialState: testState,
+describe('EditSiteNoteScreen', () => {
+  test('renders if data exists', () => {
+    const screen = render(<EditSiteNoteScreen noteId="note1" siteId="1" />, {
+      route: 'EDIT_SITE_NOTE',
+      initialState: testState,
+    });
+
+    expect(screen.getByText('Site Note')).toBeOnTheScreen();
+    expect(screen.getByDisplayValue('note 1 contents')).toBeOnTheScreen();
+    expect(mockedNavigate).toHaveBeenCalledTimes(0);
   });
 
-  expect(screen.getByText('Site Note')).toBeOnTheScreen();
-  expect(screen.getByDisplayValue('note 1 contents')).toBeOnTheScreen();
-  expect(mockedNavigate).toHaveBeenCalledTimes(0);
-});
+  test('renders null if site missing', () => {
+    const screen = render(
+      <EditSiteNoteScreen noteId="note1" siteId="nonexistent-site" />,
+      {
+        route: 'EDIT_SITE_NOTE',
+        initialState: testState,
+      },
+    );
 
-test('renders null if site missing', () => {
-  const screen = render(
-    <EditSiteNoteScreen noteId="note1" siteId="nonexistent-site" />,
-    {
-      route: 'EDIT_SITE_NOTE',
-      initialState: testState,
-    },
-  );
+    // Ideally would want to test that navigation worked, but I can't figure out how to do that
+    expect(screen.queryByText('Site Note')).toBeNull();
+    expect(screen.queryByText('note 1 contents')).toBeNull();
+    expect(mockedNavigate).toHaveBeenCalledTimes(1);
+    expect(mockedNavigate).toHaveBeenCalledWith('BOTTOM_TABS');
+  });
 
-  // Ideally would want to test that navigation worked, but I can't figure out how to do that
-  expect(screen.queryByText('Site Note')).toBeNull();
-  expect(screen.queryByText('note 1 contents')).toBeNull();
-  expect(mockedNavigate).toHaveBeenCalledTimes(1);
-  expect(mockedNavigate).toHaveBeenCalledWith('BOTTOM_TABS');
-});
+  test('renders null if note missing', () => {
+    const screen = render(
+      <EditSiteNoteScreen noteId="nonexistent-note" siteId="1" />,
+      {
+        route: 'EDIT_SITE_NOTE',
+        initialState: testState,
+      },
+    );
 
-test('renders null if note missing', () => {
-  const screen = render(
-    <EditSiteNoteScreen noteId="nonexistent-note" siteId="1" />,
-    {
-      route: 'EDIT_SITE_NOTE',
-      initialState: testState,
-    },
-  );
-
-  expect(screen.queryByText('Site Note')).toBeNull();
-  expect(screen.queryByText('note 1 contents')).toBeNull();
-  expect(mockedNavigate).toHaveBeenCalledTimes(1);
-  expect(mockedNavigate).toHaveBeenCalledWith('SITE_TABS', {
-    initialTab: 'NOTES',
-    siteId: '1',
+    expect(screen.queryByText('Site Note')).toBeNull();
+    expect(screen.queryByText('note 1 contents')).toBeNull();
+    expect(mockedNavigate).toHaveBeenCalledTimes(1);
+    expect(mockedNavigate).toHaveBeenCalledWith('SITE_TABS', {
+      initialTab: 'NOTES',
+      siteId: '1',
+    });
   });
 });
