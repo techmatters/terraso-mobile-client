0c0fe0a6512b30743038e7c2d481f4add2750621
Ruxandra Machedon <ruxandra@techmatters.org>
2024-10-30 12:14:40 -0400

    fix: only use sync manager with offline flag

--- dev-client/src/App.tsx
+++ dev-client/src/App.tsx
@@ -44,6 +44,7 @@ import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 import {captureConsoleIntegration} from '@sentry/core';
 import * as Sentry from '@sentry/react-native';
 
+import {RestrictByFlag} from 'terraso-mobile-client/components/RestrictByFlag';
 import {APP_CONFIG} from 'terraso-mobile-client/config';
 import {ForegroundPermissionsProvider} from 'terraso-mobile-client/context/AppPermissionsContext';
 import {ConnectivityContextProvider} from 'terraso-mobile-client/context/connectivity/ConnectivityContext';
@@ -133,7 +134,9 @@ function App(): React.JSX.Element {
                         <SitesScreenContextProvider>
                           <ConnectivityContextProvider>
                             <ForegroundPermissionsProvider>
-                              <SyncManager />
+                              <RestrictByFlag flag="FF_offline">
+                                <SyncManager />
+                              </RestrictByFlag>
                               <RootNavigator />
                             </ForegroundPermissionsProvider>
                           </ConnectivityContextProvider>
