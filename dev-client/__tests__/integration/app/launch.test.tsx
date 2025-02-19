69299d2a7af37e6cadea13cf64c6a78494046ecd
shrouxm <mushrouxm@gmail.com>
2025-02-19 12:03:06 -0800

    chore: add some more tests

--- dev-client/__tests__/integration/app/launch.test.tsx
+++ dev-client/__tests__/integration/app/launch.test.tsx
@@ -3,8 +3,17 @@ import {render} from '@testing/integration/utils';
 
 import {AppContent} from 'terraso-mobile-client/app/AppContent';
 
+// NOTE: these tests depend on each other and must be run in the same
+//   order because they rely on global KV store state. there is a tech
+//   debt issue to make the kvStorage instanced to each test
 describe('app launch flows', () => {
   test('welcome screen displays on initial app launch', async () => {
+    const app = render(<AppContent />);
+
+    expect(app.getByTestId('welcome-screen')).toBeOnTheScreen();
+  });
+
+  test('welcome screen displays on second app launch', async () => {
     const app = render(<AppContent />);
 
     expect(app.getByTestId('welcome-screen')).toBeOnTheScreen();
@@ -16,5 +25,26 @@ describe('app launch flows', () => {
     fireEvent.press(app.getByRole('button', {name: 'Get started'}));
 
     expect(app.getByTestId('login-screen')).toBeOnTheScreen();
+  });
+
+  test('welcome screen no longer displays after navigating to login screen', async () => {
+    const app = render(<AppContent />);
+
+    expect(app.getByTestId('login-screen')).toBeOnTheScreen();
+  });
+
+  test('sites screen displays when launching with user data in store', async () => {
+    const app = render(<AppContent />, {
+      initialState: {
+        account: {
+          currentUser: {
+            // @ts-ignore
+            data: {},
+          },
+        },
+      },
+    });
+
+    expect(app.getByTestId('sites-screen')).toBeOnTheScreen();
   });
 });
