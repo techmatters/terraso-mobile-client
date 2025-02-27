91a1687e7993395902f70f3afc40ce05eae14512
Carissa Knipe <carissa@techmatters.org>
2025-02-27 14:26:32 -0800

    chore: enable Prettier formatting for test (and mock) files

--- dev-client/__tests__/integration/OfflineSnackbar.test.tsx
+++ dev-client/__tests__/integration/OfflineSnackbar.test.tsx
@@ -18,22 +18,22 @@
 import 'terraso-mobile-client/translations';
 import 'terraso-mobile-client/config';
 
-import { Button } from 'react-native';
-import { PaperProvider, Portal } from 'react-native-paper';
+import {Button} from 'react-native';
+import {PaperProvider, Portal} from 'react-native-paper';
 
-import { act, fireEvent } from '@testing-library/react-native';
-import { testState } from '@testing/integration/data';
-import { render } from '@testing/integration/utils';
+import {act, fireEvent} from '@testing-library/react-native';
+import {testState} from '@testing/integration/data';
+import {render} from '@testing/integration/utils';
 
 import * as projectService from 'terraso-client-shared/project/projectService';
 
-import { OfflineSnackbar } from 'terraso-mobile-client/components/messages/OfflineSnackbar';
-import { FeatureFlagName } from 'terraso-mobile-client/config/featureFlags';
+import {OfflineSnackbar} from 'terraso-mobile-client/components/messages/OfflineSnackbar';
+import {FeatureFlagName} from 'terraso-mobile-client/config/featureFlags';
 import * as connectivityHooks from 'terraso-mobile-client/hooks/connectivityHooks';
-import { deleteProject } from 'terraso-mobile-client/model/project/projectSlice';
-import { ProjectListScreen } from 'terraso-mobile-client/screens/ProjectListScreen/ProjectListScreen';
-import { ProjectViewScreen } from 'terraso-mobile-client/screens/ProjectViewScreen/ProjectViewScreen';
-import { AppState, useDispatch } from 'terraso-mobile-client/store';
+import {deleteProject} from 'terraso-mobile-client/model/project/projectSlice';
+import {ProjectListScreen} from 'terraso-mobile-client/screens/ProjectListScreen/ProjectListScreen';
+import {ProjectViewScreen} from 'terraso-mobile-client/screens/ProjectViewScreen/ProjectViewScreen';
+import {AppState, useDispatch} from 'terraso-mobile-client/store';
 
 jest.mock('terraso-mobile-client/config/featureFlags', () => {
   const actual = jest.requireActual(
@@ -189,20 +189,19 @@ describe('Offline snackbar', () => {
     );
 
     // FYI: Because the snackbar has an animation transition before hiding and returning null,
-    // Need to run the jest timers, and  wrap it in act() to make sureresulting state updates 
+    // Need to run the jest timers, and  wrap it in act() to make sureresulting state updates
     // are fully applied
     act(() => {
       jest.runAllTimers();
-    })
+    });
 
     expect(screen.queryByTestId(snackbarTestId)).not.toBeOnTheScreen();
   });
 
-
   test('can be dismissed, and is shown when thunk fails offline', async () => {
     console.log('------ TEST: dismiss + shown when thunk fails offline ------');
     useIsOfflineMock.mockReturnValue(true);
-    
+
     const projectId = '1';
     const initialState = {
       project: {
@@ -221,7 +220,7 @@ describe('Offline snackbar', () => {
         },
       },
     } as Partial<AppState>;
-    
+
     // Button that simulates dispatching something to server
     deleteProjectMock.mockReturnValue(Promise.reject(['rejected']));
     const TestButton = () => {
@@ -239,7 +238,7 @@ describe('Offline snackbar', () => {
     };
 
     const screen = render(
-        <PaperProvider>
+      <PaperProvider>
         <Portal.Host>
           <TestButton />
           <OfflineSnackbar />
@@ -248,45 +247,45 @@ describe('Offline snackbar', () => {
       </PaperProvider>,
       {route: 'PROJECT_VIEW', initialState},
     );
-    console.log("2")
+    console.log('2');
 
     const snackbar = screen.queryByTestId(snackbarTestId);
     expect(snackbar).toBeOnTheScreen();
- 
+
     // Dismiss snackbar
     fireEvent(snackbar, 'onDismiss');
 
-    // Question 0: If we await act, the entire suite fails in the same way as in Question 1 below. 
+    // Question 0: If we await act, the entire suite fails in the same way as in Question 1 below.
     // Why would that make a difference?
     act(() => {
-      jest.runAllTimers()
-    })
-    console.log("3")
+      jest.runAllTimers();
+    });
+    console.log('3');
 
     // Question 1: Why would `expect(undefined).toBeTruthy();` here cause the entire suite to fail, not just the single test?
-    //   If put further up in the test, or in other tests, it only fails the single test, not the entire suite. 
+    //   If put further up in the test, or in other tests, it only fails the single test, not the entire suite.
     //
-    // Output from `npm run test OfflineSnackbar`: 
+    // Output from `npm run test OfflineSnackbar`:
     //   Test suite failed to run
     //   TypeError: Cannot set properties of undefined (setting 'message')
     //     at node_modules/jest-circus/build/formatNodeAssertErrors.js:47:25
     //         at Array.map (<anonymous>)
     //   Test Suites: 1 failed, 1 total
     //   Tests:       0 total
-    // I suspect this is the erroring line in the test library: 
+    // I suspect this is the erroring line in the test library:
     // https://github.com/jestjs/jest/blob/main/packages/jest-circus/src/formatNodeAssertErrors.ts#L58
     expect(screen.queryByTestId(snackbarTestId)).not.toBeOnTheScreen();
 
-
-    // Fire the test button event to make a server request. The request is mocked to fail, 
+    // Fire the test button event to make a server request. The request is mocked to fail,
     // which should add a message to the notificationsSlice and trigger the snackbar
     fireEvent.press(screen.queryByTestId('test-delete-project-btn'));
     await act(() => {
       jest.runAllTimers();
     });
-    console.log("4")
 
-    // Question 2: When should an act call be awaited or not? 
+    console.log('4');
+
+    // Question 2: When should an act call be awaited or not?
     // The expect below fails if we do not await the act above -- even though the IDE says `await` has
     // no effect on the expression. I had thought you'd only need to await if the function passed in
     // is async, but perhaps I'm wrong. Why would this make a difference at all, and what is special about
@@ -296,7 +295,7 @@ describe('Offline snackbar', () => {
     //   received value must be a host element.
     //   Received has value: null
     expect(screen.queryByTestId(snackbarTestId)).toBeOnTheScreen();
-});
+  });
 
   // test('is not shown when thunk fails online', () => {});
   // test('is not shown when thunk succeeds online', () => {});
