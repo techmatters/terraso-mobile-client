92a5611f5e161a020bb50564c1e7d169f4908a7a
Carissa Knipe <carissa@techmatters.org>
2025-07-17 14:28:28 -0700

    fix: use expo-build-properties to set targetSdkVersion to 35 for android

--- dev-client/app.config.ts
+++ dev-client/app.config.ts
@@ -158,6 +158,16 @@ const defaultConfig: ExpoConfig = {
         return modConfig;
       }) as any,
     ],
+    [
+      'expo-build-properties',
+      {
+        android: {
+          compileSdkVersion: 35, // TODO-cknipe: Do we need this? Will it default to the highest if left out?
+          targetSdkVersion: 35,
+          buildToolsVersion: '35.0.0',
+        },
+      },
+    ],
   ],
   extra: ENV_CONFIG,
 };
