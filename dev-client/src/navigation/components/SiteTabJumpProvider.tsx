04b45d4a509d1674f75040c2d6ac69e3ff0405b7
Johannes <89488492+johannesparty@users.noreply.github.com>
2025-08-28 09:51:14 -0700

    fix: nav stack changes 2936 (#3040)
    * fix: change navigation to avoid loops 2936

---
+++ dev-client/src/navigation/components/SiteTabJumpProvider.tsx
@@ -0,0 +1,50 @@
+/*
+ * Copyright © 2025 Technology Matters
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+import React, {createContext, ReactNode, useContext, useState} from 'react';
+
+import {SiteTabName} from 'terraso-mobile-client/navigation/navigators/SiteTabNavigator';
+
+type SiteTabJumpContextType = {
+  nextSiteTab: SiteTabName | null;
+  setNextSiteTab: (tab: SiteTabName | null) => void;
+};
+
+// Create the context (null initially)
+const SiteTabJumpContext = createContext<SiteTabJumpContextType | null>(null);
+
+// Provider component
+export function SiteTabJumpContextProvider({children}: {children: ReactNode}) {
+  const [nextSiteTab, setNextSiteTab] = useState<SiteTabName | null>(null);
+
+  return (
+    <SiteTabJumpContext.Provider value={{nextSiteTab, setNextSiteTab}}>
+      {children}
+    </SiteTabJumpContext.Provider>
+  );
+}
+
+// A convenience hook so you don’t repeat `useContext`
+export function useSiteTabJumpContext() {
+  const ctx = useContext(SiteTabJumpContext);
+  if (!ctx) {
+    throw new Error(
+      'useSiteTabJumpContext must be used inside a SiteTabJumpContext',
+    );
+  }
+  return ctx;
+}
