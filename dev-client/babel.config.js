dd995f48ea9ce3e76f7e6aff6da4204a3d6b492c
Johannes Schmidt <89488492+johannesparty@users.noreply.github.com>
2025-10-28 17:19:15 -0700

    fix: temporarily disable Babel cache to force CI rebuild

--- dev-client/babel.config.js
+++ dev-client/babel.config.js
@@ -17,7 +17,7 @@
 
 // Babel configuration for React Native with Expo
 module.exports = api => {
-  api.cache(true);
+  api.cache(false);
 
   return {
     presets: ['babel-preset-expo', '@babel/preset-typescript'],
