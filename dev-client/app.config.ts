8851e1977d6efe6ffb36f4cf11ff050c838a0731
Carissa Knipe <carissa@techmatters.org>
2026-03-18 11:12:27 -0700

    fix: update version to 1.4.4

--- dev-client/app.config.ts
+++ dev-client/app.config.ts
@@ -92,7 +92,7 @@ if (typeof APP_BUILD === 'string') {
 const defaultConfig: ExpoConfig = {
   name: 'LandPKS Soil ID',
   slug: 'landpks',
-  version: '1.4.3',
+  version: '1.4.4',
   newArchEnabled: true,
   orientation: 'portrait',
   splash: {
